a010756afcfcffd5ccaf425160b9a8091ccb0be7
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2010-05-26 10:11:50 +0000

    Work around problems with missing hash
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@361 941e2c2b-deac-454f-805a-451daa25f33c

--- libhfst/src/implementations/SFST/src/sgi.h
+++ libhfst/src/implementations/SFST/src/sgi.h
@@ -1,4 +1,3 @@
-
 /*******************************************************************/
 /*                                                                 */
 /*     File: sgi.h                                                 */
@@ -6,45 +5,58 @@
 /*  Purpose:                                                       */
 /*  Created: Thu Sep 11 15:58:25 2008                              */
 /* Modified: Fri Sep 12 08:17:03 2008 (schmid)                     */
-/*                                                                 */
+/* Modified: Wed May 26 12:54:00 2010 (hfst)                       */
 /*******************************************************************/
 
 #ifndef _SGI_INCLUDED
 #define _SGI_INCLUDED
 
-#ifdef SGIext
+#if HAVE_CONFIG_H
+#  include <config.h>
+#endif
 
-#include <ext/hash_map>
-#include <ext/hash_set>
+#if HAVE_BACKWARD_HASH_MAP
+#  include <hash_map>
+#elif HAVE_EXT_HASH_MAP
+#  include <ext/hash_map>
+#elif HAVE_HASH_MAP
+#  include <hash_map>
+#else
+#error "missing hash_map"
+#endif
+#if HAVE_BACKWARD_HASH_SET
+#  include <hash_set>
+#elif HAVE_EXT_HASH_SET
+#  include <ext/hash_set>
+#elif HAVE_HASH_SET
+#  include <hash_set>
+#else
+#error "missing hash_set"
+#endif
+
 // Hfst addition
 namespace SFST 
 {
-
-using std::hash_map;
-using std::hash_set;
-using std::hash;
-}
+// from <http://gcc.gnu.org/onlinedocs/libstdc++/manual/backwards.html>
+#ifdef __GNUC__
+#  if __GNUC__ < 3
+  using ::hash_map;
+  using ::hash_set;
+  using ::hash;
+#  elif __GNUC__ == 3 && __GNUC_MINOR__ == 0
+  using std::hash_map;
+  using std::hash_set;
+  using std::hash;
+#  else 
+  using __gnu_cxx::hash_map;
+  using __gnu_cxx::hash_set;
+  using __gnu_cxx::hash;
+#  endif
 #else
-
-#ifdef SGI__gnu_cxx
-
-#include <ext/hash_map>
-#include <ext/hash_set>
-
-#else
-
-#include <backward/hash_map>
-#include <backward/hash_set>
-
+  using std::hash_map;
+  using std::hash_set;
+  using std::hash;
 #endif
-// Hfst addition
-namespace SFST 
-{
-
-using __gnu_cxx::hash_map;
-using __gnu_cxx::hash_set;
-using __gnu_cxx::hash;
 }
-#endif
 
 #endif
