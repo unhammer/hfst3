1c03f853d85261fc07e3bfc556f1d7373f9ed1b0
bcroom <bcroom@941e2c2b-deac-454f-805a-451daa25f33c>
2010-07-14 19:44:10 +0000

    Initial commit of bridge classes for the HFST-optimized-lookup backend
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@503 941e2c2b-deac-454f-805a-451daa25f33c

---
+++ libhfst/src/implementations/HfstOlTransducer.h
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <fstream>
+#include "HfstExceptions.h"
+#include "optimized-lookup/transducer.h"
+
+namespace hfst { namespace implementations
+{
+  using std::ostream;
+  using std::ofstream;
+  using std::istream;
+  using std::ifstream;
+  using std::string;
+  using namespace hfst::exceptions;
+  
+  class HfstOlInputStream
+  {
+  private:
+    std::string filename;
+    ifstream i_stream;
+    istream &input_stream;
+    bool weighted;
+    void skip_identifier_version_3_0(void);
+    void skip_hfst_header(void);
+  public:
+    HfstOlInputStream(bool weighted);
+    HfstOlInputStream(const char * filename, bool weighted);
+    void open(void);
+    void close(void);
+    bool is_open(void) const;
+    bool is_eof(void) const;
+    bool is_bad(void) const;
+    bool is_good(void) const;
+    bool is_fst(void) const;
+    bool operator() (void) const;
+    hfst_ol::Transducer * read_transducer(bool has_header);
+    
+    // 1=unweighted, 2=weighted
+    static int is_fst(FILE * f);
+    static int is_fst(istream &s);
+  };
+  
+  class HfstOlOutputStream 
+  {
+  private:
+    std::string filename;
+    ofstream o_stream;
+    ostream &output_stream;
+    bool weighted;
+    void write_3_0_library_header(std::ostream &out);
+  public:
+    HfstOlOutputStream(bool weighted);
+    HfstOlOutputStream(const char * filename, bool weighted);
+    void open(void);
+    void close(void);
+    void write_transducer(hfst_ol::Transducer * transducer);
+  };
+  
+  class HfstOlTransducer
+  {
+   public:
+    static hfst_ol::Transducer * create_empty_transducer(bool weighted);
+  };
+  
+} }
