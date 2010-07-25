5146324ee7de867d7ee063bc9c572770dcc91c18
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2010-07-25 00:04:50 +0000

    Include licensing blurb in every single file since it's very important
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@519 941e2c2b-deac-454f-805a-451daa25f33c

--- tools/src/hfst-commandline.h
+++ tools/src/hfst-commandline.h
@@ -7,6 +7,18 @@
  * rather they must be defined on per program basis. For reference
  * implementation see file @c hfst-skeleton.cc.
  */
+//       This program is free software: you can redistribute it and/or modify
+//       it under the terms of the GNU General Public License as published by
+//       the Free Software Foundation, version 3 of the License.
+//
+//       This program is distributed in the hope that it will be useful,
+//       but WITHOUT ANY WARRANTY; without even the implied warranty of
+//       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//       GNU General Public License for more details.
+//
+//       You should have received a copy of the GNU General Public License
+//       along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 /* These variables should be used in all command line programs.
  * In some cases they may be nonsensical; just define something then.
