6cef6549e39977f9b00778dec484a4a853e6bb90
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2010-07-25 00:46:52 +0000

    Few sample tests for tools in automated tests
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@520 941e2c2b-deac-454f-805a-451daa25f33c

---
+++ test/tools/hfst-check-help.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+for f in ../../tools/src/hfst-* ; do
+    if [ -x "$f" ] ; then
+        if ! "$f" --help > help.out ; then
+            rm help.out
+            echo $f has broken help
+            exit 1
+        fi
+        if ! grep -m 1 '^Usage: .*hfst-' help.out > /dev/null ; then
+            rm help.out
+            echo $f has malformed help
+            exit 1
+        fi
+    fi
+done
+rm help.out
+
---
+++ test/tools/hfst-check-version.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+for f in ../../tools/src/hfst-* ; do
+    if [ -x "$f" ] ; then
+        if ! "$f" --version > version.out ; then
+            rm version.out
+            echo $f has broken version
+            exit 1
+        fi
+        if ! grep -m 1 '^hfst-[^ ]\+ [0-9.]\+' version.out > /dev/null ; then
+            rm version.out
+            echo $f has malformed version
+            exit 1
+        fi
+    fi
+done
+rm version.out
+
