e4e615d73d580cc2bcbce976d8ef46a4d78b7706
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2010-07-25 00:04:50 +0000

    Include licensing blurb in every single file since it's very important
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@519 941e2c2b-deac-454f-805a-451daa25f33c

--- tools/src/hfst-commandline.cc
+++ tools/src/hfst-commandline.cc
@@ -4,6 +4,18 @@
  * @brief some utils for handling common tasks in command line programs.
  * @todo maybe replace with glib?
  */
+//       This program is free software: you can redistribute it and/or modify
+//       it under the terms of the GNU General Public License as published by
+//       the Free Software Foundation, version 3 of the License.
+//
+//       This program is distributed in the hope that it will be useful,
+//       but WITHOUT ANY WARRANTY; without even the implied warranty of
+//       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//       GNU General Public License for more details.
+//
+//       You should have received a copy of the GNU General Public License
+//       along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 #if HAVE_CONFIG_H
 #  include <config.h>
--- tools/src/hfst-common-unary-cases.h
+++ tools/src/hfst-common-unary-cases.h
@@ -1,4 +1,16 @@
-  // common-unary-cases.h
+  //       This program is free software: you can redistribute it and/or modify
+//       it under the terms of the GNU General Public License as published by
+//       the Free Software Foundation, version 3 of the License.
+//
+//       This program is distributed in the hope that it will be useful,
+//       but WITHOUT ANY WARRANTY; without even the implied warranty of
+//       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//       GNU General Public License for more details.
+//
+//       You should have received a copy of the GNU General Public License
+//       along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// common-unary-cases.h
   case 'i':
   inputfilename = hfst_strdup(optarg);
   if (strcmp(inputfilename, "-") == 0) {
