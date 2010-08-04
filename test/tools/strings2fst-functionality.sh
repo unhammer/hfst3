547588f74e65ba16c25c6d1e8bd054cd23800b5d
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2010-08-04 11:25:20 +0000

    test strings2fst and fail
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@550 941e2c2b-deac-454f-805a-451daa25f33c

---
+++ test/tools/strings2fst-functionality.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+if ! ../../tools/src/hfst-strings2fst < cat.strings > test.hfst ; then
+    exit 1
+fi
+if ! ../../tools/src/hfst-compare test.hfst cat.hfst > /dev/null 2>&1 ; then
+    exit 1
+fi
+rm test.hfst
