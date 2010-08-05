cbfc0725c998a4f2a3b333fde4ec77c6d41d19ef
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2010-08-05 12:28:13 +0000

    Ubuntu (dash?) workaround
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@561 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/empty-input.sh
+++ test/tools/empty-input.sh
@@ -1,11 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 for f in ../../tools/src/hfst-* ; do
     if [ -x "$f" ] ; then
+        echo $f
         $f < empty-file > /dev/null 2>&1
-        if [ $? == 139 ] ; then
+        if [ $? -eq 139 ] ; then
             echo $f received SEGV on empty input file
             exit 1
-        elif [ $? -gt 11 ] ; then
+        elif [ $? -gt 1 ] ; then
             echo $f did not die with SUCCESS nor FAILURE on empty input
             exit 1
         fi
