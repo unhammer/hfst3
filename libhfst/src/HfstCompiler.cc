fc3ca04377821cca65fa1af2387ccfca4699e502
eaxelson <eaxelson@941e2c2b-deac-454f-805a-451daa25f33c>
2010-08-05 15:04:47 +0000

    more functions added to hfst-calculate
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@562 941e2c2b-deac-454f-805a-451daa25f33c

--- libhfst/src/HfstCompiler.cc
+++ libhfst/src/HfstCompiler.cc
@@ -75,7 +75,12 @@ namespace hfst
   { return SFST::character_code(uc); }
   
   HfstCompiler::Character HfstCompiler::symbol_code( char *s )
-  { return SFST::symbol_code(s); }
+  { // In SFST programming language epsilon is denoted as "<>"
+    // but in HFST as "@_EPSILON_SYMBOL_@". That is why it must be
+    // treated separately here.
+    if (strcmp(s,"<>") == 0)
+      return 0;
+    return SFST::symbol_code(s); }
   
   unsigned int HfstCompiler::utf8toint( char *s )
   { return SFST::utf8toint(s); }
@@ -108,25 +113,146 @@ namespace hfst
     return rsvar_value(name);
   }
 
-  HfstTransducer * HfstCompiler::make_mapping( Ranges *r1, Ranges *r2, ImplementationType type )
+
+  HfstCompiler::Contexts *make_context( HfstTransducer *l, HfstTransducer *r )
   {
+    if (l->get_type() != r->get_type()) {
+      printf("ERROR: in hfst-compiler.yy: context transducers do not have the same type.\n");
+      exit(1);
+    }
+    ImplementationType type = l->get_type();
+
+    if (l == NULL)
+      l = new HfstTransducer(type);
+    if (r == NULL)
+      r = new HfstTransducer(type);
+    
+    HfstCompiler::Contexts *c=new HfstCompiler::Contexts();
+    c->left = l;
+    c->right = r;
+    c->next = NULL;
+    
+    return c;
+  }
+
+  HfstCompiler::Contexts *add_context( HfstCompiler::Contexts *nc, HfstCompiler::Contexts *c )    
+  {
+    if (nc->left->get_type() != c->left->get_type() || 
+	nc->right->get_type() != c->right->get_type() ) {
+      printf("ERROR: in hfst-compiler.yy: context transducers do not have the same type.\n");
+      exit(1);
+    }
+    nc->next = c;
+    return nc;
+  }
+
+  HfstTransducer * HfstCompiler::negation( HfstTransducer *t )    
+  {
+    //if (RS.size() > 0 || RSS.size() > 0)
+    //  cerr << "\nWarning: agreement operation inside of negation!\n";
+    //if (!SFST::Alphabet_Defined)
+    //  error("Negation requires the definition of an alphabet");
+
+    // go through all symbol pairs in TheAlphabet and copy them to sps
+    StringPairSet sps;
+    for( SFST::Alphabet::const_iterator it=SFST::TheAlphabet.begin(); it!=SFST::TheAlphabet.end(); it++ ) {
+      SFST::Label l=*it;
+      sps.insert(StringPair( SFST::TheAlphabet.code2symbol(l.lower_char()),
+			     SFST::TheAlphabet.code2symbol(l.upper_char())) );
+    }
+    // construct a universal language transducer
+    HfstTransducer * pi_star = new HfstTransducer(sps, t->get_type());
+    pi_star->repeat_star();
+    pi_star->subtract(*t);
+    delete t;
+    return pi_star;
+  }
+
+  // TODO
+  HfstTransducer * HfstCompiler::explode( HfstTransducer *t ) {
+    return t;
+  }
+  
+  HfstTransducer * HfstCompiler::replace_in_context(HfstTransducer * mapping, Repl_Type repl_type, Contexts *contexts, bool optional) {
+    
+    HfstTransducerPair tr_pair(*(contexts->left), *(contexts->right));
+    StringPairSet sps;
+    for( SFST::Alphabet::const_iterator it=SFST::TheAlphabet.begin(); it!=SFST::TheAlphabet.end(); it++ ) {
+      SFST::Label l=*it;
+      sps.insert(StringPair( SFST::TheAlphabet.code2symbol(l.lower_char()),
+			     SFST::TheAlphabet.code2symbol(l.upper_char())) );
+    } 
+    switch (repl_type) 
+      {
+      case SFST::repl_up:
+	return new HfstTransducer(rules::replace_up(tr_pair, *mapping, optional, sps));
+	break;
+      case SFST::repl_down:
+	return new HfstTransducer(rules::replace_down(tr_pair, *mapping, optional, sps));
+	break;
+      case SFST::repl_left:
+	return new HfstTransducer(rules::replace_left(tr_pair, *mapping, optional, sps));
+	break;
+      case SFST::repl_right:
+	return new HfstTransducer(rules::replace_right(tr_pair, *mapping, optional, sps));
+	break;
+      }
+    return NULL;
+  }
+
+  HfstTransducer * HfstCompiler::replace(HfstTransducer * mapping, Repl_Type repl_type, bool optional) {
+    
+    StringPairSet sps;
+    for( SFST::Alphabet::const_iterator it=SFST::TheAlphabet.begin(); it!=SFST::TheAlphabet.end(); it++ ) {
+      SFST::Label l=*it;
+      sps.insert(StringPair( SFST::TheAlphabet.code2symbol(l.lower_char()),
+			     SFST::TheAlphabet.code2symbol(l.upper_char())) );
+    } 
+    switch (repl_type) 
+      {
+      case SFST::repl_up:
+	return new HfstTransducer(rules::replace_up(*mapping, optional, sps));
+	break;
+      case SFST::repl_down:
+	return new HfstTransducer(rules::replace_down(*mapping, optional, sps));
+	break;
+      default:
+	return NULL;
+      }
+  }
+
+  HfstTransducer * HfstCompiler::make_mapping( Ranges *r1, Ranges *r2, ImplementationType type ) {
     return new HfstTransducer(type);
   }
   
-  HfstTransducer * HfstCompiler::result( HfstTransducer *t, bool)
-  {
+  HfstTransducer * HfstCompiler::result( HfstTransducer *t, bool) {
     t->minimize(); 
     return t;
   }
   
-  void HfstCompiler::def_alphabet( HfstTransducer *t )
+  void HfstCompiler::def_alphabet( HfstTransducer *tr )
   {
-    t->convert(SFST_TYPE);
-    cerr << *t;
-    SFST::def_alphabet(t->implementation.sfst);      // def_alphabet deletes its argument
-    t->implementation.sfst = new SFST::Transducer(); // double delete is avoided here
-    delete t;
-    return;
+    // explode, minimize
+
+    SFST::TheAlphabet.clear_char_pairs();
+    //TheAlphabet.copy(t->alphabet);
+
+    HfstMutableTransducer t(*tr);
+    HfstStateIterator state_it(t);
+    while (not state_it.done()) 
+      {
+	HfstState s = state_it.value();
+	HfstTransitionIterator transition_it(t,s);
+	while (not transition_it.done()) 
+	  {
+	    HfstTransition tr = transition_it.value();
+	    SFST::TheAlphabet.insert(SFST::Label(SFST::TheAlphabet.symbol2code(tr.isymbol.c_str()),
+						 SFST::TheAlphabet.symbol2code(tr.osymbol.c_str())));
+	    transition_it.next();
+	  }
+	state_it.next();
+      }
+    SFST::Alphabet_Defined = 1;
   }
-  
+
 }
