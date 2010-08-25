7f24b22bd0a72dc7bc71dd3c47a2a24d9f749559
eaxelson <eaxelson@941e2c2b-deac-454f-805a-451daa25f33c>
2010-08-25 14:49:44 +0000

    some functions added to HfstInternalTransducer
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@640 941e2c2b-deac-454f-805a-451daa25f33c

--- libhfst/src/implementations/InternalTransducer.cc
+++ libhfst/src/implementations/InternalTransducer.cc
@@ -57,5 +57,41 @@ namespace hfst {
       }
     }
 
+    void HfstInternalTransducer::substitute(void (*func)(std::string &isymbol, std::string &osymbol)) 
+    {
+      for (unsigned int i=0; i<lines.size(); i++) {
+	if (not lines[i].final_line) {
+	  std::string istring(alphabet->code2symbol(lines[i].isymbol));
+	  std::string ostring(alphabet->code2symbol(lines[i].osymbol));
+	  func(istring, ostring);
+	  lines[i].isymbol = alphabet->add_symbol(istring.c_str());
+	  lines[i].osymbol = alphabet->add_symbol(ostring.c_str()); 
+	}
+      }
+    }
+    
+    /* zero weights inserted, does not handle non-final states that lead nowhere */
+    void HfstInternalTransducer::insert_freely(const StringPair &symbol_pair) 
+    {
+      HfstInternalTransducer new_transducer;
+      std::set<unsigned int> visited_states;
+      unsigned int in = alphabet->add_symbol(symbol_pair.first.c_str());
+      unsigned int out = alphabet->add_symbol(symbol_pair.second.c_str());
+
+      for (std::vector<InternalTransducerLine>::iterator it = lines.begin(); 
+	   it != lines.end(); it++) {	
+	if (visited_states.find(it->origin) != visited_states.end()) {
+	  visited_states.insert(it->origin);
+	  new_transducer.add_line(it->origin, it->origin, in, out, 0);
+	}
+	if (it->final_line)
+	  new_transducer.add_line(it->origin, it->weight);
+	else
+	  new_transducer.add_line(it->origin, it->target, it->isymbol, it->osymbol, it->weight);
+      }
+      lines.clear(); // is this needed?
+      lines=new_transducer.lines;
+    }
+    
   }
 }
