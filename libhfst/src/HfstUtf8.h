aae916c6d34e6701b63d7e1c4acb8d0bf63b8efe
eaxelson <eaxelson@941e2c2b-deac-454f-805a-451daa25f33c>
2010-10-11 10:39:32 +0000

    missing files added
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@691 941e2c2b-deac-454f-805a-451daa25f33c

---
+++ libhfst/src/HfstUtf8.h
@@ -0,0 +1,22 @@
+
+/*******************************************************************/
+/*                                                                 */
+/*     File: utf8.h                                                */
+/*   Author: Helmut Schmid                                         */
+/*  Purpose:                                                       */
+/*  Created: Mon Sep  5 17:49:16 2005                              */
+/* Modified: Mon Apr  7 08:26:39 2008 (schmid)                     */
+/*                                                                 */
+/*******************************************************************/
+
+#ifndef _HFST_UTF8_H_
+#define _HFST_UTF8_H_
+
+namespace HfstUtf8 {
+  
+  unsigned int utf8toint( char *s );
+  unsigned int utf8toint( char **s );
+  char *int2utf8( unsigned int );
+  
+}
+#endif
