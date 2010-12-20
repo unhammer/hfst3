20dc849ecaeb9373b1ec047dbfee51b1cffc0222
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2010-12-20 06:04:01 +0000

    testing results of parameter vcombinations
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@830 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/compare-functionality.sh
+++ test/tools/compare-functionality.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-for f in *.hfst ; do
+comparables="cat.hfst dog.hfst tac.hfst cat2dog.hfst dog2cat.hfst "\
+    "cat_or_dog.hfst catdog.hfst"
+for f in $comparables; do
     if ! ../../tools/src/hfst-compare $f $f > /dev/null 2>&1 ; then
         echo hfst-compare mismatches $f $f
         exit 1
     fi
-    for g in *.hfst ; do
+    for g in $comparables ; do
         if test $f != $g ; then
             if hfst-compare $f $g > /dev/null 2>&1 ; then
                 echo hfst-compare matches $f $g
