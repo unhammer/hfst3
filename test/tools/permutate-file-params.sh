992775f6eb90df60b8e39ab4389b8e01aaaafac3
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2010-12-20 06:04:01 +0000

    testing results of parameter vcombinations
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@830 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/permutate-file-params.sh
+++ test/tools/permutate-file-params.sh
@@ -1,4 +1,31 @@
 #!/bin/sh
+if [ -x ../../tools/src/hfst-compare ] ; then
+    # well, not all permutations, but reasonable
+    if hfst-compare -1 cat.hfst -2 dog.hfst ; then
+       exit 1
+    fi
+    if hfst-compare -1 cat.hfst dog.hfst ; then
+       exit 1
+    fi
+    if hfst-compare -2 dog.hfst cat.hfst ; then
+       exit 1
+    fi
+    if hfst-compare dog.hfst -1 cat.hfst ; then
+       exit 1
+    fi
+    if hfst-compare cat.hfst -2 dog.hfst ; then
+       exit 1
+    fi
+    if hfst-compare cat.hfst < dog.hfst ; then
+        exit 1
+    fi
+    if hfst-compare -1 cat.hfst < dog.hfst ; then
+        exit 1
+    fi
+    if hfst-compare -2 dog.hfst < cat.hfst ; then
+        exit 1
+    fi
+fi
 for f in ../../tools/src/hfst-{conjunct,disjunct,compose,subtract} ; do
     if [ -x "$f" ] ; then
         # well, not all permutations, but reasonable
@@ -20,7 +47,52 @@ for f in ../../tools/src/hfst-{conjunct,disjunct,compose,subtract} ; do
         $f cat.hfst -o test_file1stdin2stdout.hfst  < dog.hfst || exit 1
         $f -1 cat.hfst -o test_named1stdin2namedout.hfst < dog.hfst  || exit 1
         $f -2 dog.hfst -o test_named2stdin1namedout.hfst < cat.hfst  || exit 1
+        for g in test_*.hfst ; do
+            for h in test_*.hfst ; do
+                if ! hfst-compare $g $h ; then
+                    echo "$f builds $g and $h differently from same sources"
+                    exit 1
+                fi
+            done
+        done
         rm test_*.hfst
     fi
 done
+
+for f in ../../tools/src/hfst-{determinize,invert,minimize,remove-epsilons,reverse} ; do
+    if [ -x $f ] ; then
+        $f -i cat.hfst > test_namedinstdout.hfst || exit 1
+        $f cat.hfst > test_fileinstdout.hfst || exit 1
+        $f -i cat.hfst -o test_namedinnamedout.hfst || exit 1
+        $f cat.hfst -o test_fileinnamedout.hfst || exit 1
+        for g in test_*.hfst ; do
+            for h in test_*.hfst ; do
+                if ! hfst-compare $g $h ; then
+                    echo "$f builds $g and $h differently from same sources"
+                    exit 1
+                fi
+            done
+        done
+        rm test_*.hfst
+    fi
+done
+
+for f in ../../tools/src/hfst-{fst2strings,fst2txt} ; do
+    if [ -x $f ] ; then
+        $f -i cat.hfst > test_namedinstdout.txt || exit 1
+        $f cat.hfst > test_fileinstdout.txt || exit 1
+        $f -i cat.hfst -o test_namedinnamedout.txt || exit 1
+        $f cat.hfst -o test_fileinnamedout.txt || exit 1
+        for g in test_*.txt ; do
+            for h in test_*.txt ; do
+                if diff $g $h ; then
+                    echo "$f builds $g and $h differently from same sources"
+                    exit 1
+                fi
+            done
+        done
+        rm test_*.txt
+    fi
+done
+
 
