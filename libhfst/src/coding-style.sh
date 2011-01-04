54077fa56bf3aab3aa0838e3e150259aaa17f940
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2011-01-04 07:40:11 +0000

    The alpha testing instructions
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@854 941e2c2b-deac-454f-805a-451daa25f33c

--- libhfst/src/coding-style.sh
+++ libhfst/src/coding-style.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 rv=0
 for f in *.cc *.h implementations/*.cc implementations/*.h ; do \
-    if grep -n '.{81,}' $f ; then \
+    if egrep -m 5 -n '.{81,}' $f ; then \
         echo "Overlong lines in $f!" ; \
         rv=1 ;\
     fi ; \
-    if fgrep -n '	' $f ; then \
+    if fgrep -m 5 -n '	' $f ; then \
         echo "Literal tabs in $f!" ; \
         rv=1 ; \
     fi ; \
