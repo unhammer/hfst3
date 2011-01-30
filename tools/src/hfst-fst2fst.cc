453d54cff31a77112c3d371c950802ac423bbb36
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2011-01-30 22:51:30 +0000

    Fix verbose messages about HFST3 headers
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@911 941e2c2b-deac-454f-805a-451daa25f33c

--- tools/src/hfst-fst2fst.cc
+++ tools/src/hfst-fst2fst.cc
@@ -53,123 +53,121 @@ bool hfst_format = true;
 void
 print_usage()
 {
-	// c.f. http://www.gnu.org/prep/standards/standards.html#g_t_002d_002dhelp
-	fprintf(message_out, "Usage: %s [OPTIONS...] [INFILE]\n"
-		"Convert transducers between binary formats\n"
-		"\n", program_name);
+    // c.f. http://www.gnu.org/prep/standards/standards.html#g_t_002d_002dhelp
+    fprintf(message_out, "Usage: %s [OPTIONS...] [INFILE]\n"
+        "Convert transducers between binary formats\n"
+        "\n", program_name);
 
-	print_common_program_options(message_out);
-	print_common_unary_program_options(message_out);
-	// fprintf(message_out, (tool-specific options and short descriptions)
+    print_common_program_options(message_out);
+    print_common_unary_program_options(message_out);
     fprintf(message_out, "Conversion options:\n"
-	"  -f, --format=FMT                  Write result in FMT format\n"
-	"  -b, --use-backend-format          Write result in implementation format, without any HFST wrappers\n"
-        " shorter options for defining format:\n"
-	"  -S, --sfst                        Write output in (HFST's) SFST implementation\n"
-	"  -F, --foma                        Write output in (HFST's) foma implementation\n"
-	"  -t, --tropical-weight             Write output in (HFST's) tropical weight (OpenFST) implementation\n"
-	"  -l, --log-weight                  Write output in (HFST's) log weight (OpenFST) implementation\n"
-	"  -O, --optimized-lookup            Write output in the HFST optimized-lookup implementation\n"
-	"  -w, --optimized-lookup-weighted   Write output in optimized-lookup (weighted) implementation\n");
-	fprintf(message_out, "\n");
-	print_common_unary_program_parameter_instructions(message_out);
+    "  -f, --format=FMT                  Write result in FMT format\n"
+    "  -b, --use-backend-format          Write result in implementation format, without any HFST wrappers\n"
+    "  -S, --sfst                        Write output in (HFST's) SFST implementation\n"
+    "  -F, --foma                        Write output in (HFST's) foma implementation\n"
+    "  -t, --tropical-weight             Write output in (HFST's) tropical weight (OpenFST) implementation\n"
+    "  -l, --log-weight                  Write output in (HFST's) log weight (OpenFST) implementation\n"
+    "  -O, --optimized-lookup            Write output in the HFST optimized-lookup implementation\n"
+    "  -w, --optimized-lookup-weighted   Write output in optimized-lookup (weighted) implementation\n");
+    fprintf(message_out, "\n");
+    print_common_unary_program_parameter_instructions(message_out);
         fprintf(message_out, 
             "FMT must be name of a format usable by libhfst, i.e. one of the following:\n"
-	    "{ openfst-tropical, openfst-log, sfst, foma,\n"
-	    "  optimized-lookup-weighted, optimized-lookup-unweighted }.\n");
-	fprintf(message_out, "\n");
-	print_report_bugs();
+        "{ openfst-tropical, openfst-log, sfst, foma,\n"
+        "  optimized-lookup-weighted, optimized-lookup-unweighted }.\n");
     fprintf(message_out, "\n");
-	print_more_info();
+    print_report_bugs();
+    fprintf(message_out, "\n");
+    print_more_info();
 }
 
 int
 parse_options(int argc, char** argv)
 {
-	// use of this function requires options are settable on global scope
-	while (true)
-	{
-		static const struct option long_options[] =
-		{
-		  HFST_GETOPT_COMMON_LONG,
-		  HFST_GETOPT_UNARY_LONG,
-		  // add tool-specific options here 
-		  {"use-backend-format", no_argument, 0, 'b'},
-		  {"format",       required_argument, 0, 'f'},
-		  {"sfst",               no_argument, 0, 'S'},
-		  {"foma",               no_argument, 0, 'F'},
-		  {"tropical-weight",    no_argument, 0, 't'},
-		  {"log-weight",         no_argument, 0, 'l'},
-		  {"optimized-lookup",   no_argument, 0, 'O'},
-		  {"optimized-lookup-weighted",no_argument, 0, 'w'},
-		  {0,0,0,0}
-		};
-		int option_index = 0;
-		// add tool-specific options here 
-		char c = getopt_long(argc, argv, HFST_GETOPT_COMMON_SHORT
+    // use of this function requires options are settable on global scope
+    while (true)
+    {
+        static const struct option long_options[] =
+        {
+          HFST_GETOPT_COMMON_LONG,
+          HFST_GETOPT_UNARY_LONG,
+          // add tool-specific options here 
+          {"use-backend-format", no_argument, 0, 'b'},
+          {"format",       required_argument, 0, 'f'},
+          {"sfst",               no_argument, 0, 'S'},
+          {"foma",               no_argument, 0, 'F'},
+          {"tropical-weight",    no_argument, 0, 't'},
+          {"log-weight",         no_argument, 0, 'l'},
+          {"optimized-lookup",   no_argument, 0, 'O'},
+          {"optimized-lookup-weighted",no_argument, 0, 'w'},
+          {0,0,0,0}
+        };
+        int option_index = 0;
+        // add tool-specific options here 
+        char c = getopt_long(argc, argv, HFST_GETOPT_COMMON_SHORT
                              HFST_GETOPT_UNARY_SHORT "SFtlOwf:b",
-							 long_options, &option_index);
-		if (-1 == c)
-		{
-			break;
-		}
+                             long_options, &option_index);
+        if (-1 == c)
+        {
+            break;
+        }
 
 
-		switch (c)
-		{
+        switch (c)
+        {
 #include "inc/getopt-cases-common.h"
 #include "inc/getopt-cases-unary.h"
-		  // add tool-specific cases here
+          // add tool-specific cases here
                 case 'f':
-		  output_type = hfst_parse_format_name(optarg);
-		  break;
-		case 'b':
-		  hfst_format=false;
-		  break;
-		case 'S':
-		  output_type = hfst::SFST_TYPE;
-		  break;
-		case 'F':
-		  output_type = hfst::FOMA_TYPE;
-		  break;
-		case 't':
-		  output_type = hfst::TROPICAL_OFST_TYPE;
-		  break;
-		case 'l':
-		  output_type = hfst::LOG_OFST_TYPE;
-		  break;
-		case 'O':
-		  output_type = hfst::HFST_OL_TYPE;
-		  break;
-		case 'w':
-		  output_type = hfst::HFST_OLW_TYPE;
-		  break;
+          output_type = hfst_parse_format_name(optarg);
+          break;
+        case 'b':
+          hfst_format=false;
+          break;
+        case 'S':
+          output_type = hfst::SFST_TYPE;
+          break;
+        case 'F':
+          output_type = hfst::FOMA_TYPE;
+          break;
+        case 't':
+          output_type = hfst::TROPICAL_OFST_TYPE;
+          break;
+        case 'l':
+          output_type = hfst::LOG_OFST_TYPE;
+          break;
+        case 'O':
+          output_type = hfst::HFST_OL_TYPE;
+          break;
+        case 'w':
+          output_type = hfst::HFST_OLW_TYPE;
+          break;
 #include "inc/getopt-cases-error.h"
-		}
-	}
-	
-	if (output_type == hfst::UNSPECIFIED_TYPE)
-	{
-		error(EXIT_FAILURE, 0, 
+        }
+    }
+    
+    if (output_type == hfst::UNSPECIFIED_TYPE)
+    {
+        error(EXIT_FAILURE, 0, 
               "You must specify an output type "
               "(one of -S, -f, -t, -l, -O, or -w)");
-	}
+    }
 
 #include "inc/check-params-common.h"
 #include "inc/check-params-unary.h"
-	return EXIT_CONTINUE;
+    return EXIT_CONTINUE;
 }
 
 int
 process_stream(HfstInputStream& instream, HfstOutputStream& outstream)
 {
   //instream.open();
-  //	outstream.open();
-	
-	size_t transducer_n = 0;
-	while(instream.is_good())
-	{
-		transducer_n++;
+  //    outstream.open();
+    
+    size_t transducer_n = 0;
+    while(instream.is_good())
+    {
+        transducer_n++;
         if(transducer_n == 1)
         {
           verbose_printf("Converting %s...\n", inputfilename); 
@@ -179,84 +177,64 @@ process_stream(HfstInputStream& instream, HfstOutputStream& outstream)
           verbose_printf("Converting %s...%zu\n",
                          inputfilename, transducer_n);
         }
-		HfstTransducer orig(instream);
-		outstream << orig.convert(output_type);
-	}
-	instream.close();
-	outstream.close();
-	return EXIT_SUCCESS;
+        HfstTransducer orig(instream);
+        outstream << orig.convert(output_type);
+    }
+    instream.close();
+    outstream.close();
+    return EXIT_SUCCESS;
 }
 
 
 int main( int argc, char **argv ) {
     hfst_set_program_name(argv[0], "0.1", "HfstFst2Fst");
     int retval = parse_options(argc, argv);
-	if (retval != EXIT_CONTINUE)
-	{
-		return retval;
-	}
-	// close buffers, we use streams
-	if (inputfile != stdin)
-	{
-		fclose(inputfile);
-	}
-	if (outfile != stdout)
-	{
-		fclose(outfile);
-	}
-	verbose_printf("Reading from %s, writing to %s\n", 
-		inputfilename, outfilename);
-	switch (output_type)
+    if (retval != EXIT_CONTINUE)
+    {
+        return retval;
+    }
+    // close buffers, we use streams
+    if (inputfile != stdin)
+    {
+        fclose(inputfile);
+    }
+    if (outfile != stdout)
+    {
+        fclose(outfile);
+    }
+    verbose_printf("Reading from %s, writing to %s\n", 
+        inputfilename, outfilename);
+    char* format_description = hfst_strformat(output_type);
+    if (hfst_format) 
       {
-      case hfst::SFST_TYPE:
-        verbose_printf("Writing SFST format transducers "
-                       "(with HFST header)\n");
-        break;
-      case hfst::TROPICAL_OFST_TYPE:
-        verbose_printf("Writing OpenFst format transducers, tropical weights "
-                      "(with HFST header)\n");
-        break;
-      case hfst::LOG_OFST_TYPE:
-        verbose_printf("Writing OpenFst format transducers, log weights "
-                      "(with HFST header)\n");
-        break;
-      case hfst::FOMA_TYPE:
-        verbose_printf("Writing foma format transducers "
-                       "(with HFST header)\n");
-        break;
-      case hfst::HFST_OL_TYPE:
-        verbose_printf("Writing HFST optimized lookup transducers, "
-                       "unweighted\n");
-        break;
-      case hfst::HFST_OLW_TYPE:
-        verbose_printf("Writing HFST optimized lookup transducers, "
-                       "weighted\n");
-        break;
-      default:
-        error(EXIT_FAILURE, 0, "Unknown output format\n");
-        return EXIT_FAILURE;
-        break;
+        verbose_printf("Writing %s format transducers with HFST3 headers\n",
+                       format_description);
       }
-
+    else
+      {
+        verbose_printf("Writing %s format transducers without HFST specific"
+                       " headers\n", format_description);
+      }
+    free(format_description);
     // here starts the buffer handling part
-	HfstInputStream* instream = NULL;
-	try {
-	  instream = (inputfile != stdin) ?
-	    new HfstInputStream(inputfilename) : new HfstInputStream();
-	} catch (NotTransducerStreamException)	{
-		error(EXIT_FAILURE, 0, "%s is not a valid transducer file",
+    HfstInputStream* instream = NULL;
+    try {
+      instream = (inputfile != stdin) ?
+        new HfstInputStream(inputfilename) : new HfstInputStream();
+    } catch (NotTransducerStreamException)  {
+        error(EXIT_FAILURE, 0, "%s is not a valid transducer file",
               inputfilename);
-		return EXIT_FAILURE;
-	}
-	HfstOutputStream* outstream = (outfile != stdout) ?
-	  new HfstOutputStream(outfilename, output_type, hfst_format) :
-	  new HfstOutputStream(output_type, hfst_format);
-	
-	retval = process_stream(*instream, *outstream);
-	delete instream;
-	delete outstream;
-	free(inputfilename);
-	free(outfilename);
-	return retval;
+        return EXIT_FAILURE;
+    }
+    HfstOutputStream* outstream = (outfile != stdout) ?
+      new HfstOutputStream(outfilename, output_type, hfst_format) :
+      new HfstOutputStream(output_type, hfst_format);
+    
+    retval = process_stream(*instream, *outstream);
+    delete instream;
+    delete outstream;
+    free(inputfilename);
+    free(outfilename);
+    return retval;
 }
 
