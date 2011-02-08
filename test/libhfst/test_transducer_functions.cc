57a8da46e6943d8c5ccc8d9b9ff4bbd6de385b3c
eaxelson <eaxelson@941e2c2b-deac-454f-805a-451daa25f33c>
2011-02-08 15:06:12 +0000

    More tests added.
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@931 941e2c2b-deac-454f-805a-451daa25f33c

--- test/libhfst/test_transducer_functions.cc
+++ test/libhfst/test_transducer_functions.cc
@@ -1,5 +1,39 @@
 /*
-   Test file for HfstTransducer member functions.
+   Test file for HfstTransducer member functions:
+   - compare
+   - compose
+   - convert
+   - extract_strings and extract_strings_fd
+   - insert_freely
+   - is_cyclic
+   - is_lookup_infinitely_ambiguous, lookup and lookup_fd
+   - n_best
+   - push_weights
+   - set_final_weights
+   - substitute
+   - transform_weights
+   
+   Functions:
+      concatenate 
+      determinize 
+      disjunct 
+      input_project 
+      intersect 
+      invert 
+      minimize 
+      optionalize 
+      output_project 
+      remove_epsilons 
+      repeat_n 
+      repeat_n_minus 
+      repeat_n_plus 
+      repeat_n_to_k 
+      repeat_plus 
+      repeat_star 
+      reverse 
+      subtract 
+   are tested in command line tool tests
+
 */
 
 #include "HfstTransducer.h"
@@ -73,57 +107,127 @@ int main(int argc, char **argv)
       }
 
 
-      /* Function concatenate. */
-
       /* Function convert. */
       {
-	/*
 	verbose_print("function convert", types[i]);
 
 	HfstTransducer t1("foo", "bar", types[i]);
-	for (unsigned int j=0; j<; j++)
+	HfstTransducer t2("foo", "bar", types[i]);
+	for (unsigned int j=0; j<=TYPES_SIZE; j++)
 	  {
 	    t1.convert(types[(i+j)%TYPES_SIZE]);
 	  }
-	*/
+	assert(t1.compare(t2));
       }
 
 
-      /* determinize. */
-      /* disjunct. */
-      /* extract_strings. */
-      /* extract_string_fd. */
-      /* input_project. */
-      /* insert_freely. */
-      /* insert_freely. */
-      /* intersect. */
-      /* invert. */
-      /* is_cyclic. */
-      /* is_lookup_infinitely_ambiguous. */
-      /* lookup. */
-      /* lookup_fd. */
-      /* minimize. */
-      /* n_best. */
-      /* optionalize. */
-      /* output_project. */
-      /* push_weights. */
-      /* remove_epsilons. */
-      /* repeat_n. */
-      /* repeat_n_minus. */
-      /* repeat_n_plus. */
-      /* repeat_n_to_k. */
-      /* repeat_plus. */
-      /* repeat_star. */
-      /* reverse. */
-      /* set_final_weights. */
-      /* substitute. */
-      /* substitute. */
-      /* substitute. */
-      /* substitute. */
-      /* substitute. */
-      /* subtract. */
-      /* transform_weights. */
+      /* Functions extract_strings and extract_strings_fd. */
+      {
+	verbose_print("function extract_strings(_fd)", types[i]);
+
+	using hfst::StringPair;
+	using hfst::StringPairSet;
+	using hfst::HfstTokenizer;
+	using hfst::WeightedPaths;
+
+	HfstTokenizer tok;
+	HfstTransducer cat("cat", "cats", tok, types[i]);
+	cat.set_final_weights(3);
+	HfstTransducer dog("dog", "dogs", tok, types[i]);
+	dog.set_final_weights(2.5);
+	HfstTransducer mouse("mouse", "mice",  tok, types[i]);
+	mouse.set_final_weights(1.7);
+	HfstTransducer animals(types[i]);
+	animals.disjunct(cat);
+	animals.disjunct(dog);
+	animals.disjunct(mouse);
+	animals.minimize();
+
+	StringPairSet expected_results;
+	expected_results.insert(StringPair("cat","cats"));
+	expected_results.insert(StringPair("dog","dogs"));
+	expected_results.insert(StringPair("mouse","mice"));
+
+	WeightedPaths<float>::Set results;
+	animals.extract_strings(results, 
+				3, /* max_num */ 
+				0  /* cycles */ );
+
+	/* Test that results are as expected. */
+	assert(results.size() == 3);
+	for (WeightedPaths<float>::Set::const_iterator it = results.begin();
+	     it != results.end(); it++)
+	  {
+	    StringPair sp(it->istring, it->ostring);
+	    assert(expected_results.find(sp) != expected_results.end());
+	    /* Test weights. */
+	    if (types[i] == TROPICAL_OFST_TYPE ||
+		types[i] == LOG_OFST_TYPE)
+	      {
+		/* Rounding can affect precision. */
+		if (it->istring.compare("cat") == 0)
+		  assert(it->weight > 2.99 && it->weight < 3.01);
+		else if (it->istring.compare("dog") == 0)
+		  assert(it->weight > 2.49 && it->weight < 2.51);
+		else if (it->istring.compare("mouse") == 0)
+		  assert(it->weight > 1.69 && it->weight < 1.71);
+		else
+		  assert(false);
+	      }
+	  }
 	
+      }
+
+
+#ifdef TESTS_IMPLEMENTED
+      /* Function insert_freely. */
+      {
+	verbose_print("function ...", types[i]);
+      }
+
+
+      /* Function is_cyclic. */
+      {
+	verbose_print("function ...", types[i]);
+      }
+
+
+      /* Functions is_lookup_infinitely_ambiguous, lookup and lookup_fd. */
+      {
+	verbose_print("function ...", types[i]);
+      }
+
+
+      /* Function n_best. */
+      {
+	verbose_print("function ...", types[i]);
+      }
+
+
+      /* Function push_weights. */
+      {
+	verbose_print("function ...", types[i]);
+      }
+
+
+      /* Function set_final_weights. */
+      {
+	verbose_print("function ...", types[i]);
+      }
+
+
+      /* Functions substitute. */
+      {
+	verbose_print("function ...", types[i]);
+      }
+
+
+      /* Function transform_weights. */
+      {
+	verbose_print("function ...", types[i]);
+      }
+#endif // TESTS_IMPLEMENTED
+
 
     }
 }
