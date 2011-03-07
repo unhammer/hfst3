2c559522cf844e5da76e6bd929935805a0c584c4
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2011-03-07 03:17:30 +0000

    Add tests for UTF-8 and latin-1 encoded strings
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@1071 941e2c2b-deac-454f-805a-451daa25f33c

---
+++ test/tools/latin-1-strings.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+../../tools/src/hfst-strings2fst < latin-1.strings > test.hfst
+RV=$?
+if test $RV -eq 0 ; then
+    echo Latin-1 input incorrectly allowed
+    exit 1
+elif test $RV -gt 8 ; then
+    exit 1
+fi
