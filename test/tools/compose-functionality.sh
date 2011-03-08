ad97a39a52e4d55ec3486473915070174d085b95
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2011-03-08 20:40:10 +0000

    Tests for ? symbols
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@1091 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/compose-functionality.sh
+++ test/tools/compose-functionality.sh
@@ -8,5 +8,18 @@ for i in "" .sfst .ofst .foma; do
             exit 1
         fi
         rm test.hfst;
+        if test -f identity-star.hfst$i ; then
+            if ! ../../tools/src/hfst-compose cat.hfst$i identity-star.hfst$i > test.hfst ; then
+                exit 1
+            fi
+            if ! ../../tools/src/hfst-compare cat.hfst$i test.hfst > /dev/null ; then
+                exit 1
+            fi
+        fi
+        if test -f unknown-star.hfst$i ; then
+            if ! ../../tools/src/hfst-compose cat.hfst$i identity-star.hfst$i > test.hfst ; then
+                exit 1
+            fi
+        fi
     fi
 done
--- test/tools/fst2strings-functionality.sh
+++ test/tools/fst2strings-functionality.sh
@@ -2,9 +2,11 @@
 for i in "" .sfst .ofst .foma; do
     if test -f cat.hfst$i ; then
         if ! ../../tools/src/hfst-fst2strings < cat.hfst$i > test.strings ; then
+            echo turning cat.hfst$i to strings failed
             exit 1
         fi
         if ! diff test.strings cat.strings > /dev/null 2>&1 ; then
+            echo cat.hfst$i strings differ from expected
             exit 1
         fi
         rm test.strings;
