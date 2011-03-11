e765c1042ca071a44c983eab856390ac8a6ce5e4
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2011-03-11 01:00:12 +0000

    More tests limited to available formats
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@1104 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/compose-intersect-functionality.sh
+++ test/tools/compose-intersect-functionality.sh
@@ -1,27 +1,35 @@
 #!/bin/sh
-for i in openfst-tropical foma sfst # openfst-log
-  do
-if ! echo "cat|dog" | ../../tools/src/hfst-calculate -f $i > lexicon.hfst ; then
-    exit 1
-fi
-if ! echo "([catdog]|d:D|c:C)*" | ../../tools/src/hfst-calculate -f $i > rule1.hfst ; then
-    exit 1
-fi
-if ! echo "([catdog]|d:D|c:C)*" | ../../tools/src/hfst-calculate -f $i > rule2.hfst ; then
-    exit 1
-fi
-if ! ../../tools/src/hfst-compose-intersect -l lexicon.hfst rule1.hfst rule2.hfst > TMP ; then
-    exit 1
-fi
-if ! echo "cat|dog|{cat}:{Cat}|{dog}:{Dog}" | ../../tools/src/hfst-calculate > test.hfst ; then
-    exit 1
-fi
-if ! ../../tools/src/hfst-compare test.hfst TMP > /dev/null 2>&1 ; then
-    exit 1
-fi ;
-  done ;
-rm test.hfst
-rm TMP
-rm lexicon.hfst
-rm rule1.hfst
-rm rule2.hfst
+for i in "" .sfst .ofst .foma; do
+    FFLAG=
+    case $i in
+        .sfst)
+            FFLAG="-f sfst";;
+        .ofst)
+            FFLAG="-f openfst-tropical";;
+        .foma)
+            FFLAG="-f foma";;
+        *)
+            FFLAG=;;
+    esac
+    if test -f cat.hfst$i ; then
+        if ! echo "cat|dog" | ../../tools/src/hfst-calculate $FFLAG > lexicon.hfst ; then
+            exit 1
+        fi
+        if ! echo "([catdog]|d:D|c:C)*" | ../../tools/src/hfst-calculate $FFLAG > rules.hfst ; then
+            exit 1
+        fi
+        if ! echo "([catdog]|d:D|c:C)*" | ../../tools/src/hfst-calculate $FFLAG >> rules.hfst ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-compose-intersect -1 lexicon.hfst -2 rules.hfst > test.hfst ; then
+            exit 1
+        fi
+        if ! echo "cat|dog|{cat}:{Cat}|{dog}:{Dog}" | ../../tools/src/hfst-calculate $FFLAG > test2.hfst ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-compare test.hfst test2.hfst > /dev/null 2>&1 ; then
+            exit 1
+        fi
+        rm test.hfst test2.hfst lexicon.hfst rules.hfst
+    fi
+done
