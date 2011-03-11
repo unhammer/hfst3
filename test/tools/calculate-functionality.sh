02a5bf2b0c948f73b4ea3f39bb530c26b8686608
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2011-03-11 01:00:12 +0000

    More tests limited to available formats
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@1104 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/calculate-functionality.sh
+++ test/tools/calculate-functionality.sh
@@ -1,17 +1,32 @@
 #!/bin/sh
-for i in foma openfst-tropical sfst # openfst-log
-  do
-if ! echo "catcatcat(cat)+" | ../../tools/src/hfst-calculate -f $i > test.hfst ; then
-    exit 1
-fi
-if ! ../../tools/src/hfst-fst2fst -f $i 4toINFcats.hfst | ../../tools/src/hfst-compare test.hfst > /dev/null 2>&1 ; then
-    exit 1
-fi
-if ! echo "{cat}:{dog}" | ../../tools/src/hfst-calculate -f $i > test.hfst ; then
-    exit 1
-fi
-if ! ../../tools/src/hfst-fst2fst -f $i cat2dog.hfst | ../../tools/src/hfst-compare test.hfst > /dev/null 2>&1 ; then
-    exit 1
-fi ;
-  done ;
-rm test.hfst
+for i in "" .sfst .ofst .foma; do
+    FFLAG=
+    case $i in
+        .sfst)
+            FFLAG="-f sfst";;
+        .ofst)
+            FFLAG="-f openfst-tropical";;
+        .foma)
+            FFLAG="-f foma";;
+        *)
+            FFLAG=;;
+    esac
+    if test -f 4toINFcats.hfst$i ; then
+        if ! echo "catcatcat(cat)+" | ../../tools/src/hfst-calculate $FFLAG > test.hfst ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-compare 4toINFcats.hfst$i test.hfst > /dev/null 2>&1 ; then
+            exit 1
+        fi
+        rm test.hfst
+    fi
+    if test -f cat2dog.hfst$i ; then
+        if ! echo "{cat}:{dog}" | ../../tools/src/hfst-calculate $FFLAG > test.hfst ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-compare cat2dog.hfst$i test.hfst > /dev/null 2>&1 ; then
+            exit 1
+        fi
+        rm test.hfst
+    fi
+done
