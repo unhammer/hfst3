c9114cd0f6ae4cf39ba964063a3b1c83ae9c6777
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2011-03-11 00:35:58 +0000

    Test all string formats in strings2fst
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@1102 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/strings2fst-functionality.sh
+++ test/tools/strings2fst-functionality.sh
@@ -1,42 +1,56 @@
 #!/bin/sh
-if ! ../../tools/src/hfst-strings2fst -f sfst < cat.strings > test.hfst ; then
-    exit 1
-fi
-if ! ../../tools/src/hfst-compare test.hfst cat.hfst.sfst > /dev/null 2>&1 ; then
-    exit 1
-fi
-rm test.hfst
-if ! ../../tools/src/hfst-strings2fst -f sfst -S < c_a_t.strings > test.hfst ; then
-    exit 1
-fi
-if ! ../../tools/src/hfst-compare test.hfst cat.hfst.sfst > /dev/null 2>&1 ; then
-    exit 1
-fi
+for i in "" .sfst .ofst .foma; do
+    FFLAG=
+    case $i in
+        .sfst)
+            FFLAG="-f sfst";;
+        .ofst)
+            FFLAG="-f openfst-tropical";;
+        .foma)
+            FFLAG="-f foma";;
+        *)
+            FFLAG=;;
+    esac
+    if test -f cat.hfst$i ; then
+        if ! ../../tools/src/hfst-strings2fst $FFLAG < cat.strings > test.hfst ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-compare test.hfst cat.hfst$i > /dev/null 2>&1 ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-strings2fst $FFLAG -S < c_a_t.strings > test.hfst ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-compare test.hfst cat.hfst$i > /dev/null 2>&1 ; then
+            exit 1
+        fi
+        rm test.hfst
+    fi
+    if test -f cat2dog.hfst$i ; then
+        if ! ../../tools/src/hfst-strings2fst $FFLAG < cat2dog.strings > test.hfst ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-compare test.hfst cat2dog.hfst$i > /dev/null 1>&1 ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-strings2fst $FFLAG -S < cat2dog.spaces > test.hfst ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-compare test.hfst cat2dog.hfst$i > /dev/null 1>&1 ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-strings2fst $FFLAG -p -S < cat2dog.pairs > test.hfst ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-compare test.hfst cat2dog.hfst$i > /dev/null 1>&1 ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-strings2fst $FFLAG -p < cat2dog.pairstring > test.hfst ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-compare test.hfst cat2dog.hfst$i > /dev/null 1>&1 ; then
+            exit 1
+        fi
+    fi
+done
 
-if ! ../../tools/src/hfst-strings2fst -f openfst-tropical < cat.strings > test.hfst ; then
-    exit 1
-fi
-if ! ../../tools/src/hfst-compare test.hfst cat.hfst.ofst > /dev/null 2>&1 ; then
-    exit 1
-fi
-rm test.hfst
-if ! ../../tools/src/hfst-strings2fst -f openfst-tropical -S < c_a_t.strings > test.hfst ; then
-    exit 1
-fi
-if ! ../../tools/src/hfst-compare test.hfst cat.hfst.ofst > /dev/null 2>&1 ; then
-    exit 1
-fi
-
-if ! ../../tools/src/hfst-strings2fst -f foma < cat.strings > test.hfst ; then
-    exit 1
-fi
-if ! ../../tools/src/hfst-compare test.hfst cat.hfst.foma > /dev/null 2>&1 ; then
-    exit 1
-fi
-rm test.hfst
-if ! ../../tools/src/hfst-strings2fst -f foma -S < c_a_t.strings > test.hfst ; then
-    exit 1
-fi
-if ! ../../tools/src/hfst-compare test.hfst cat.hfst.foma > /dev/null 2>&1 ; then
-    exit 1
-fi
