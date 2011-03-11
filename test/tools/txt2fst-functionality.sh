fdb88eb0a3f8a27113bd6dc26858f7d3cd193fa9
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2011-03-11 00:45:23 +0000

    Only test available automata formats
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@1103 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/txt2fst-functionality.sh
+++ test/tools/txt2fst-functionality.sh
@@ -1,24 +1,23 @@
 #!/bin/sh
-if ! ../../tools/src/hfst-txt2fst -f sfst < cat.txt > test.hfst ; then
-    exit 1
-fi
-if ! ../../tools/src/hfst-compare test.hfst cat.hfst.sfst > /dev/null 2>&1 ; then
-    exit 1
-fi
-rm test.hfst
-
-if ! ../../tools/src/hfst-txt2fst -f openfst-tropical < cat.txt > test.hfst ; then
-    exit 1
-fi
-if ! ../../tools/src/hfst-compare test.hfst cat.hfst.ofst > /dev/null 2>&1 ; then
-    exit 1
-fi
-rm test.hfst
-
-if ! ../../tools/src/hfst-txt2fst -f foma < cat.txt > test.hfst ; then
-    exit 1
-fi
-if ! ../../tools/src/hfst-compare test.hfst cat.hfst.foma > /dev/null 2>&1 ; then
-    exit 1
-fi
-rm test.hfst
+for i in "" .sfst .ofst .foma; do
+    FFLAG=
+    case $i in
+        .sfst)
+            FFLAG="-f sfst";;
+        .ofst)
+            FFLAG="-f openfst-tropical";;
+        .foma)
+            FFLAG="-f foma";;
+        *)
+            FFLAG=;;
+    esac
+    if test -f cat.hfst$i ; then
+        if ! ../../tools/src/hfst-txt2fst $FFLAG < cat.txt > test.hfst ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-compare test.hfst cat.hfst$i > /dev/null 2>&1 ; then
+            exit 1
+        fi
+        rm test.hfst
+    fi
+done
