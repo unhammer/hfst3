c005c68c25dac0349ec03a711a59e031ed5d39dd
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2011-03-13 19:23:25 +0000

    Fix brokentests
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@1122 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/permutate-file-params.sh
+++ test/tools/permutate-file-params.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ -x ../../tools/src/../../tools/src/hfst-compare ] ; then
+if [ -x ../../tools/src/hfst-compare ] ; then
     # well, not all permutations, but reasonable
     if ../../tools/src/hfst-compare -1 cat.hfst -2 dog.hfst > /dev/null ; then
        exit 1
@@ -26,7 +26,7 @@ if [ -x ../../tools/src/../../tools/src/hfst-compare ] ; then
         exit 1
     fi
 fi
-for f in ../../tools/src/hfst-{conjunct,disjunct,compose,subtract} ; do
+for f in ../../tools/src/hfst-{conjunct,disjunct,compose,subtract,compose,compose-intersect} ; do
     if [ -x "$f" ] ; then
         # well, not all permutations, but reasonable
         $f -1 cat.hfst -2 dog.hfst > test_named1named2stdout.hfst || exit 1
@@ -35,7 +35,7 @@ for f in ../../tools/src/hfst-{conjunct,disjunct,compose,subtract} ; do
         $f -2 dog.hfst cat.hfst > test_named2file1stdout.hfst || exit 1
         $f cat.hfst -2 dog.hfst > test_file1named2stdout.hfst || exit 1
         $f dog.hfst -1 cat.hfst > test_file2named1stdout.hfst || exit 1
-        $f cat.hfst < dog.hfst > test_file1stdin2stdout.hfst || exit 1
+        $f dog.hfst < cat.hfst > test_file1stdin2stdout.hfst || exit 1
         $f -1 cat.hfst < dog.hfst > test_named1stdin2stdout.hfst || exit 1
         $f -2 dog.hfst < cat.hfst > test_named2stdin1stdout.hfst || exit 1
         $f -1 cat.hfst -2 dog.hfst -o test_named1named2namedout.hfst || exit 1
@@ -44,7 +44,7 @@ for f in ../../tools/src/hfst-{conjunct,disjunct,compose,subtract} ; do
         $f -2 dog.hfst cat.hfst -o test_named2file1namedout.hfst || exit 1
         $f cat.hfst -2 dog.hfst -o test_file1named2namedout.hfst || exit 1
         $f dog.hfst -1 cat.hfst -o test_file2named1namedout.hfst || exit 1
-        $f cat.hfst -o test_file1stdin2stdout.hfst  < dog.hfst || exit 1
+        $f dog.hfst -o test_file1stdin2stdout.hfst  < cat.hfst || exit 1
         $f -1 cat.hfst -o test_named1stdin2namedout.hfst < dog.hfst  || exit 1
         $f -2 dog.hfst -o test_named2stdin1namedout.hfst < cat.hfst  || exit 1
         for g in test_*.hfst ; do
