99120023d478c40fbe19b9a3e025cd55a1098344
mpsilfve <mpsilfve@941e2c2b-deac-454f-805a-451daa25f33c>
2011-03-14 12:28:58 +0000

    Added HfstTokenizer utf-8 validation tests.
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@1128 941e2c2b-deac-454f-805a-451daa25f33c

--- test/libhfst/test_tokenizer.cc
+++ test/libhfst/test_tokenizer.cc
@@ -55,4 +55,172 @@ int main(int argc, char **argv)
   assert(tokenization4[1] == StringPair("b","@_EPSILON_SYMBOL_@"));
   assert(tokenization4[2] == StringPair("a","@_EPSILON_SYMBOL_@"));
 
+  // Utf-8 correctness testing.
+  HfstTokenizer utf8_tokenizer;
+  char line[100];
+
+  // Positive cases
+
+  // Empty string
+  line[0] = 0;
+  try
+    { utf8_tokenizer.check_utf8_correctness(line); }
+  catch (const IncorrectUtf8CodingException & e)
+    { assert(false); }
+
+  // ASCII string "ab".
+  line[0] = 'a';
+  line[1] = 'b';
+  line[2] = 0;
+  try
+    { utf8_tokenizer.check_utf8_correctness(line); }
+  catch (const IncorrectUtf8CodingException & e)
+    { assert(false); }
+
+  // Four byte, three byte, two byte and single byte sequence.
+  verbose_print("Case: 11110000, 10000000, 10000000, 10000000,\n");
+  verbose_print("11100000, 10000000, 10000000, 11010000, 10000000,\n");
+  verbose_print("01000000");
+
+  line[0] = get_bin(1,1,1,1,0,0,0,0);
+  line[1] = get_bin(1,0,0,0,0,0,0,0);
+  line[2] = get_bin(1,0,0,0,0,0,0,0);
+  line[3] = get_bin(1,0,0,0,0,0,0,0);
+  line[4] = get_bin(1,1,1,0,0,0,0,0);
+  line[5] = get_bin(1,0,0,0,0,0,0,0);
+  line[6] = get_bin(1,0,0,0,0,0,0,0);
+  line[7] = get_bin(1,1,0,1,0,0,0,0);
+  line[8] = get_bin(1,0,0,0,0,0,0,0);
+  line[9] = get_bin(0,1,0,0,0,0,0,0);
+  line[10]= 0;
+
+  try
+    { utf8_tokenizer.check_utf8_correctness(line); }
+  catch (const IncorrectUtf8CodingException & e)
+    { assert(false); }
+
+  // Negative Cases
+  
+  // String with byte 192.
+  line[0] = get_bin(1,1,0,0,0,0,0,0);
+  line[1] = get_bin(1,0,0,0,0,0,0,0);
+  line[2] = 0;
+  try
+    { 
+      utf8_tokenizer.check_utf8_correctness(line); 
+      assert(false);
+    }
+  catch (const IncorrectUtf8CodingException & e) {};
+
+  // String with byte 193.
+  line[0] = get_bin(1,1,0,0,0,0,0,1);
+  line[1] = get_bin(1,0,0,0,0,0,0,0);
+  line[2] = 0;
+  try
+    { 
+      utf8_tokenizer.check_utf8_correctness(line); 
+      assert(false);
+    }
+  catch (const IncorrectUtf8CodingException & e) {};
+
+  // String with byte 245.
+  line[0] = get_bin(1,1,1,1,0,1,0,1);
+  line[1] = get_bin(1,0,0,0,0,0,0,0);
+  line[2] = get_bin(1,0,0,0,0,0,0,0);
+  line[3] = get_bin(1,0,0,0,0,0,0,0);
+  line[4] = 0;
+  try
+    { 
+      utf8_tokenizer.check_utf8_correctness(line); 
+      assert(false);
+    }
+  catch (const IncorrectUtf8CodingException & e) {};
+
+  // String with byte 246.
+  line[0] = get_bin(1,1,1,1,0,1,1,0);
+  line[1] = get_bin(1,0,0,0,0,0,0,0);
+  line[2] = get_bin(1,0,0,0,0,0,0,0);
+  line[3] = get_bin(1,0,0,0,0,0,0,0);
+  line[4] = 0;
+  try
+    { 
+      utf8_tokenizer.check_utf8_correctness(line); 
+      assert(false);
+    }
+  catch (const IncorrectUtf8CodingException & e) {};
+
+  // String with byte 247.
+  line[0] = get_bin(1,1,1,1,0,1,1,1);
+  line[1] = get_bin(1,0,0,0,0,0,0,0);
+  line[2] = get_bin(1,0,0,0,0,0,0,0);
+  line[3] = get_bin(1,0,0,0,0,0,0,0);
+  line[4] = 0;
+  try
+    { 
+      utf8_tokenizer.check_utf8_correctness(line); 
+      assert(false);
+    }
+  catch (const IncorrectUtf8CodingException & e) {};
+
+  // ASCII character followed by a continuation character.
+  line[0] = get_bin(0,1,0,0,0,0,0,0);
+  line[1] = get_bin(1,0,0,0,0,0,0,0);
+  line[2] = 0;
+  try
+    { 
+      utf8_tokenizer.check_utf8_correctness(line); 
+      assert(false);
+    }
+  catch (const IncorrectUtf8CodingException & e) {};
+
+  // 11010000 followed by ASCII character
+  line[0] = get_bin(1,1,0,1,0,0,0,0);
+  line[1] = get_bin(0,1,0,0,0,0,0,0);
+  line[2] = 0;
+  try
+    { 
+      utf8_tokenizer.check_utf8_correctness(line); 
+      assert(false);
+    }
+  catch (const IncorrectUtf8CodingException & e) {};
+
+  // 11100000 followed by one continuation character and an ASCII character
+  line[0] = get_bin(1,1,1,0,0,0,0,0);
+  line[1] = get_bin(1,0,0,0,0,0,0,0);
+  line[2] = get_bin(0,1,0,0,0,0,0,0);
+  line[3] = 0;
+  try
+    { 
+      utf8_tokenizer.check_utf8_correctness(line); 
+      assert(false);
+    }
+  catch (const IncorrectUtf8CodingException & e) {};
+
+  // 11110000 followed by two continuation characters and an ASCII character
+  line[0] = get_bin(1,1,1,1,0,0,0,0);
+  line[1] = get_bin(1,0,0,0,0,0,0,0);
+  line[2] = get_bin(1,0,0,0,0,0,0,0);
+  line[3] = get_bin(0,1,0,0,0,0,0,0);
+  line[4] = 0;
+  try
+    { 
+      utf8_tokenizer.check_utf8_correctness(line); 
+      assert(false);
+    }
+  catch (const IncorrectUtf8CodingException & e) {};
+
+  // 11110000 followed by two continuation characters and 11010000,10000000
+  line[0] = get_bin(1,1,1,1,0,0,0,0);
+  line[1] = get_bin(1,0,0,0,0,0,0,0);
+  line[2] = get_bin(1,0,0,0,0,0,0,0);
+  line[3] = get_bin(1,1,0,1,0,0,0,0);
+  line[4] = get_bin(1,0,0,0,0,0,0,0);
+  line[5] = 0;
+  try
+    { 
+      utf8_tokenizer.check_utf8_correctness(line); 
+      assert(false);
+    }
+  catch (const IncorrectUtf8CodingException & e) {};
+  
 }
