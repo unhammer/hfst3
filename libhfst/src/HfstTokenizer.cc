ebe96679e8e4b23df84844c273ad2779d7029c81
mpsilfve <mpsilfve@941e2c2b-deac-454f-805a-451daa25f33c>
2011-03-14 11:21:55 +0000

    made HfstTokenizer::check_utf8_correctness a public static
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@1125 941e2c2b-deac-454f-805a-451daa25f33c

--- libhfst/src/HfstTokenizer.cc
+++ libhfst/src/HfstTokenizer.cc
@@ -196,17 +196,19 @@ StringPairVector HfstTokenizer::tokenize
 }
   
   void 
-  HfstTokenizer::check_utf8_correctness(const std::string &input_string) const
+  HfstTokenizer::check_utf8_correctness(const std::string &input_string)
 {
+  // Check that input_string is made up from utf-8 sequences.
   for (std::string::const_iterator it = input_string.begin(); 
        it != input_string.end();
        ++it)
     {
+      // The initial byte of the sequence.
       unsigned char initial_char = *it;
       size_t additional_chars = 0;
   
       // The bytes 192, 193, 245, 246 and 247 are invalid in utf8.
-      if (initial_char == 192 or initial_char == 193 or 
+      if (initial_char == 192 or initial_char == 193 or
 	  initial_char == 245 or initial_char == 246 or initial_char == 247)
 	{ HFST_THROW(IncorrectUtf8CodingException); }
       // Case 0xxxxxxx, i.e. ASCII byte.
@@ -228,13 +230,14 @@ StringPairVector HfstTokenizer::tokenize
       else
 	{ HFST_THROW(IncorrectUtf8CodingException); }
 
+      // Read the continuation bytes.
       for (size_t i = 0; i < additional_chars; ++i)
 	{
 	  ++it;
 	  // String ends too early.
 	  if (it == input_string.end())
 	    { HFST_THROW(IncorrectUtf8CodingException); }
-	  char byte = *it;
+	  unsigned char byte = *it;
 	  // All continuation bytes looka like 10xxxxxx.
 	  if (not (128 & byte and 64 ^ byte))
 	    { HFST_THROW(IncorrectUtf8CodingException); }
