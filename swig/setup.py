1e0650ead34f494e850780dd5bbd9a0b261c0a06
hardwick <hardwick@941e2c2b-deac-454f-805a-451daa25f33c>
2011-05-17 08:30:28 +0000

    Global wrapper - could it be this easy?
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@1319 941e2c2b-deac-454f-805a-451daa25f33c

--- swig/setup.py
+++ swig/setup.py
@@ -13,15 +13,22 @@ def flatten(lst):
 libhfst_src_path = '../libhfst/src/'
 
 def libsrc_and_wrap(filenames):
+    """utility function for creating per-file wrappers"""
     return list(flatten([[os.path.abspath(libhfst_src_path + filename),
                           os.path.abspath(filename.split('.')[0] + \
                                               '_wrap.cxx')]
                          for filename in filenames]))
+# could use with eg.
+#                           libsrc_and_wrap(('HfstTransducer.cc',
+#                                                     'HfstInputStream.cc',
+#                                                     'HfstOutputStream.cc')),
 
 libhfst_module = Extension('_libhfst',
-                           sources=libsrc_and_wrap(('HfstTransducer.cc',
-                                                    'HfstInputStream.cc',
-                                                    'HfstOutputStream.cc')),
+                           sources=map(lambda s: libhfst_src_path + s,
+                                       ['HfstTransducer.cc',
+                                        'HfstInputStream.cc',
+                                        'HfstOutputStream.cc']) +\
+                               ['libhfst_wrap.cxx'], # global wrapper
                            include_dirs = [os.path.abspath(libhfst_src_path)],
                            libraries = ['libhfst'],
                            library_dirs = ['/usr/local/lib'])
