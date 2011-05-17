86f87289587c046117897cdd60f7b900724d76dc
hardwick <hardwick@941e2c2b-deac-454f-805a-451daa25f33c>
2011-05-17 07:21:31 +0000

    Initial framework
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@1313 941e2c2b-deac-454f-805a-451daa25f33c

---
+++ swig/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+"""
+setup for HFST-swig
+"""
+
+import os, sys, itertools
+from distutils.core import setup, Extension
+
+def flatten(lst):
+    return itertools.chain.from_iterable(lst)
+
+libhfst_src_path = '../libhfst/src/'
+
+def libsrc_and_wrap(filenames):
+    return list(flatten([[os.path.abspath(libhfst_src_path + filename),
+                          os.path.abspath(filename.split('.')[0] + \
+                                              '_wrap.cc')]
+                         for filename in filenames]))
+
+libhfst_module = Extension('_libhfst',
+                           sources=libsrc_and_wrap(('HfstTransducer.cc',
+                                                    'HfstInputStream.cc',
+                                                    'HfstOutputStream.cc')))
+
+print libsrc_and_wrap(('HfstTransducer.cc',
+                       'HfstInputStream.cc',
+                       'HfstOutputStream.cc'))
+
+setup(name = 'libhfst_swig',
+      version = '3.0.3_beta',
+      author = 'HFST team',
+      description = 'SWIG-bound libhfst interface',
+      ext_modules = [libhfst_module],
+      py_modules = ["libhfst"]
+      )
