c018f4ae471138e1d2e2f6724235161bb7871897
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2011-05-23 12:00:21 +0000

    decrease stress by order of 8
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@1340 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/lookup-stress.sh
+++ test/tools/lookup-stress.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-for i in "" .sfst .ofst .foma; do
+for i in "" ol .sfst .ofst .foma; do
     if test -f cat2dog.hfst$i ; then
-        for c in 1 256 65536 4294967297 ; do
-            if ! yes cat | head -n $c | ../../tools/src/hfst-lookup cat2dog.hfst$i ; then
+        for c in 1 256 65536 16777216 ; do
+            echo doing $c times cat in $i
+            if ! yes cat | head -n $c | ../../tools/src/hfst-lookup cat2dog.hfst$i > /dev/null ; then
+                echo "died with $? ($PIPESTATUS)"
                 exit 1
             fi
         done
--- test/tools/proc-stress.sh
+++ test/tools/proc-stress.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-for i in "" .sfst .ofst .foma; do
-    if test -f cat2dog.hfst$i ; then
-        for c in 1 256 65536 4294967297 ; do
-            if ! yes cat | head -n $c | ../../tools/src/hfst-proc cat2dog.hfst$i ; then
-                exit 1
-            fi
-        done
-    fi
-done
+if test -f cat2dog.hfstol ; then
+    for c in 1 256 65536 16777216 ; do
+        if ! yes cat | head -n $c | ../../tools/src/hfst-proc/hfst-proc cat2dog.hfstol > /dev/null ; then
+            exit 1
+        fi
+    done
+fi
