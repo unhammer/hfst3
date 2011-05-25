77a9a18ad67ac942f7fdf57a3a8b99202e369cc6
mpsilfve <mpsilfve@941e2c2b-deac-454f-805a-451daa25f33c>
2011-05-25 09:27:30 +0000

    Fixed includes in XreCompiler.h and LexcCompiler.h
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@1350 941e2c2b-deac-454f-805a-451daa25f33c

--- libhfst/src/parsers/XreCompiler.h
+++ libhfst/src/parsers/XreCompiler.h
@@ -31,7 +31,7 @@
 #endif
 
 #include <string>
-#include "HfstDataTypes.h"
+#include "../HfstDataTypes.h"
 
 namespace hfst { 
 //! @brief hfst::xre namespace is used for all functions related to Xerox 
