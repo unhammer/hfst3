2105cd64d7f89e2d9dac92b2d6f312b8e712b003
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2011-07-25 15:20:43 +0000

    Move latin-1 string tests to XFAIL
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@1492 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/latin-1-strings.sh
+++ test/tools/latin-1-strings.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 ../../tools/src/hfst-strings2fst $srcdir/latin-1.strings > test.hfst
 RV=$?
-if test $RV -eq 0 ; then
-    echo Latin-1 input incorrectly allowed
-    exit 1
-elif test $RV -gt 8 ; then
-    exit 1
-fi
 rm test.hfst
+exit $RV
