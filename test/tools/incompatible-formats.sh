23979565c0c7d4da8b65dbedad5ea8887acc96ea
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2011-07-25 15:58:22 +0000

    incompatible format messages in composition
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@1494 941e2c2b-deac-454f-805a-451daa25f33c

---
+++ test/tools/incompatible-formats.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+for f in "" .sfst .foma .openfst; do
+    for g in "" .sfst .foma .openfst; do
+        if test cat.hfst$f != cat.hfst$g ; then
+            if test -f cat.hfst$f -a -f cat.hfst$g ; then
+                for p in compare compose ; do
+                    if ../../tools/src/hfst-$p cat.hfst$f cat.hfst$g ; then
+                        echo hfst-$p does not fail cat.hfst$f cat.hfst$g
+                        exit 0
+                    fi
+                done
+            fi
+        fi
+    done
+done
+exit 2
