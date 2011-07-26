443960b9a23baab7a7fbe6ecb51a479a6630ff47
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2011-07-26 23:27:15 +0000

    fix some -a tests
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@1501 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/subtract-functionality.sh
+++ test/tools/subtract-functionality.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for i in "" .sfst .ofst .foma; do
-    if test -f cat_or_dog.hfst$i -a -f dog.hfst$i -a cat.hfst$i ; then
+    if test -f cat_or_dog.hfst$i -a -f dog.hfst$i -a -f cat.hfst$i ; then
         if ! ../../tools/src/hfst-subtract cat_or_dog.hfst$i dog.hfst$i > test.hfst ; then
             exit 1
         fi
