66a5627a182b54d691a64b4f5bb57589f92a3ca9
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2011-08-03 09:49:39 +0000

    Mark version requirements in autogen
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@1523 941e2c2b-deac-454f-805a-451daa25f33c

--- autogen.sh
+++ autogen.sh
@@ -75,15 +75,15 @@
 
 # set to minimum acceptible version of autoconf
 if [ "x$AUTOCONF_VERSION" = "x" ] ; then
-    AUTOCONF_VERSION=2.52
+    AUTOCONF_VERSION=2.22
 fi
 # set to minimum acceptible version of automake
 if [ "x$AUTOMAKE_VERSION" = "x" ] ; then
-    AUTOMAKE_VERSION=1.6.0
+    AUTOMAKE_VERSION=1.11.0
 fi
 # set to minimum acceptible version of libtool
 if [ "x$LIBTOOL_VERSION" = "x" ] ; then
-    LIBTOOL_VERSION=1.4.2
+    LIBTOOL_VERSION=2.2.6
 fi
 
 
