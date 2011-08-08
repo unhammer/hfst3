e778b10945b238c343ec9cfdc5123864c6c7b1a0
hardwick <hardwick@941e2c2b-deac-454f-805a-451daa25f33c>
2011-08-08 11:17:49 +0000

    Full api and distutils-based build & install
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@1534 941e2c2b-deac-454f-805a-451daa25f33c

--- swig/hfst_swig_extensions.h
+++ swig/hfst_swig_extensions.h
@@ -3,30 +3,45 @@
 #include <utility>
 #include "HfstDataTypes.h"
 #include "HfstTransducer.h"
-#include "Hfst
+#include "HfstFlagDiacritics.h"
 
 typedef std::vector<std::pair <float, std::vector<std::string> > >
 OneLevelPathVector;
 
-typedef std::vector<std::pair <float, std::string> > FloatStringVector;
+typedef std::vector<std::pair <std::string, float> > StringFloatVector;
 
 namespace hfst {
 
-OneLevelPathVector vectorize(HfstOneLevelPaths * holps, bool remove_flags = true)
+OneLevelPathVector vectorize(HfstOneLevelPaths * holps)
 {
     std::vector<std::pair <float, std::vector<std::string> > > retval;
     for (HfstOneLevelPaths::const_iterator it = holps->begin();
 	 it != holps->end(); ++it) {
-	if (
 	retval.push_back(*it);
     }
     return retval;
 }
 
-
-FloatStringVector detokenize_vector(OneLevelPathVector olpv)
+OneLevelPathVector purge_flags(OneLevelPathVector olpv)
 {
-    FloatStringVector retval;
+    OneLevelPathVector retval;
+    for (OneLevelPathVector::const_iterator it = olpv.begin(); it != olpv.end();
+	 ++it) {
+	std::vector<std::string> temp;
+	for (std::vector<std::string>::const_iterator str_it =
+		 it->second.begin(); str_it != it->second.end(); ++str_it) {
+	    if (!FdOperation::is_diacritic(*str_it)) {
+		    temp.push_back(*str_it);
+		}
+	}
+	retval.push_back(std::pair<float, std::vector<std::string> >(it->first, temp));
+    }
+    return retval;
+}
+
+StringFloatVector detokenize_vector(OneLevelPathVector olpv)
+{
+    StringFloatVector retval;
     for (OneLevelPathVector::const_iterator it = olpv.begin(); it != olpv.end();
 	 ++it) {
 	std::string temp;
@@ -34,12 +49,12 @@ FloatStringVector detokenize_vector(OneLevelPathVector olpv)
 		 it->second.begin(); str_it != it->second.end(); ++str_it) {
 	    temp.append(*str_it);
 	}
-	retval.push_back(std::pair<float, std::string>(it->first, temp));
+	retval.push_back(std::pair<std::string, float>(temp, it->first));
     }
     return retval;
 }
 
-FloatStringVector detokenize_paths(HfstOneLevelPaths * holps)
+StringFloatVector detokenize_paths(HfstOneLevelPaths * holps)
 {
     return detokenize_vector(vectorize(holps));
 }
