7510897750fad166082ebcce3db35163005fd062
mpsilfve <mpsilfve@941e2c2b-deac-454f-805a-451daa25f33c>
2011-09-09 13:57:12 +0000

    Initial add of hfst-tagger. A lot of stuff is still missing and the tool needs to be integrated in the build system.
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@1630 941e2c2b-deac-454f-805a-451daa25f33c

---
+++ tools/src/hfst-tagger/hfst_tagger_code_skeleton.cc
@@ -0,0 +1,18 @@
+#include "X.h"
+
+#ifdef HFST_TAGGER_TEST_X
+#include <cassert>
+#include <iostream>
+int main(void)
+{
+  std::cerr << "Unit test for: " << __FILE__ << "\t";
+
+  // Unit test begins.
+
+
+
+  // Unit test ends.
+
+  std::cerr << "PASSED" << std::endl;
+}
+#endif // HFST_TAGGER_TEST_X
