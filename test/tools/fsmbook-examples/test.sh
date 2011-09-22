b83f8d2316b5303f0645ffd55eeb851f0a926045
eaxelson <eaxelson@941e2c2b-deac-454f-805a-451daa25f33c>
2011-09-22 11:48:57 +0000

    Added more fsmbook examples. The tests have to wait until we get rules in hfst-regexp2fst.
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@1647 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/fsmbook-examples/test.sh
+++ test/tools/fsmbook-examples/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# Rules must be implemented before FinnishProsody can be tested 
 for i in DateParser EinsteinsPuzzle EnglishNumerals \
-FinnishNumerals NumbersToNumerals; do \
+FinnishNumerals NumbersToNumerals Palindroms; do \
 cd $i; 
 if !(make check); then
     exit 1
