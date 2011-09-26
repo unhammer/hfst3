cfa6cd320ff07286d3b2ac7ec48924bd7445499e
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2011-09-26 13:56:22 +0000

    few less apertiums in directories
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@1658 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/proc-stress.sh
+++ test/tools/proc-stress.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if test -f cat2dog.hfstol ; then
     for c in 1 256 65536 16777216 ; do
-        if ! yes cat | head -n $c | ../../tools/src/hfst-apertium-proc/hfst-apertium-proc cat2dog.hfstol > /dev/null ; then
+        if ! yes cat | head -n $c | ../../tools/src/hfst-proc/hfst-apertium-proc cat2dog.hfstol > /dev/null ; then
             exit 1
         fi
     done
