d6d8fe06ec7ab8ecf06c2b40bc38c43bc96ced74
eaxelson <eaxelson@941e2c2b-deac-454f-805a-451daa25f33c>
2011-09-26 13:09:14 +0000

    Added a test case, Lingala. The test is not included until an issue with writing transducers in AT&T format with the limited licence of xfst.
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@1656 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/fsmbook-examples/test.sh
+++ test/tools/fsmbook-examples/test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Rules must be implemented before directories FinnishProsody, PlusOrMinus
 # and FinnishOTProsody can be tested 
+# Directory Lingala requires a professional licence of xfst so that
+# the resulting transducer can be written in att format. 
 for i in DateParser EinsteinsPuzzle EnglishNumerals \
 FinnishNumerals NumbersToNumerals Palindroms; do \
 cd $i; 
