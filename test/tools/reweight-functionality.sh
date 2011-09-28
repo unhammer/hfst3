e82b0cbe8d691c61ec7a62737ada76fe9bc0da24
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2011-09-28 07:36:53 +0000

    Add tool for modifying weights
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@1673 941e2c2b-deac-454f-805a-451daa25f33c

---
+++ test/tools/reweight-functionality.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+for i in "" .sfst .ofst .foma; do
+    if test -f heavycat.hfst$i ; then
+        if ! ../../tools/src/hfst-reweight -a 1 heavycat.hfst$i > test.hfst ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-reweight -b 3.141 heavycat.hfst$i > test.hfst ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-reweight -a 10 -b 0.001 heavycat.hfst$i > test.hfst ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-reweight -F cos heavycat.hfst$i > test.hfst ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-reweight -a 11 -b 0.5 -F sin heavycat.hfst$i > test.hfst ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-reweight -l 1000 -a 1 heavycat.hfst$i > test.hfst ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-reweight -u 100000 -a 1 heavycat.hfst$i > test.hfst ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-reweight -l 1000 -u 100000 -a 1 heavycat.hfst$i > test.hfst ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-reweight -S c -a 1 heavycat.hfst$i > test.hfst ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-reweight -I c -a 1 heavycat.hfst$i > test.hfst ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-reweight -O c -a 1 heavycat.hfst$i > test.hfst ; then
+            exit 1
+        fi
+        if ! ../../tools/src/hfst-reweight -I c -O c -a 1 heavycat.hfst$i > test.hfst ; then
+            exit 1
+        fi
+    fi
+done
+
