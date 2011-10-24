4e70bd51f6c3c095253d7446f14d6e507bc88eff
eaxelson <eaxelson@941e2c2b-deac-454f-805a-451daa25f33c>
2011-10-24 14:59:10 +0000

    Fixed a bug in hfst-format. Also fixed some tests so that they don't assume that all backend libraries are available.
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@1767 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/format-functionality.sh
+++ test/tools/format-functionality.sh
@@ -1,34 +1,36 @@
 #!/bin/sh
-if ! echo "a:b" | ../../tools/src/hfst-calculate -f sfst > test.hfst ; then
-    exit 1
+if echo "a:b" | ../../tools/src/hfst-calculate -f sfst > test.hfst ; then
+    if ! ../../tools/src/hfst-format test.hfst > TMP1 ; then
+	exit 1
+    fi
+    echo "Transducers in test.hfst are of type SFST (1.4 compatible)" > TMP2
+    if ! diff TMP1 TMP2 ; then
+	exit 1
+    fi
 fi
-if ! ../../tools/src/hfst-format test.hfst > TMP1 ; then
-    exit 1
+
+if echo "a:b" | ../../tools/src/hfst-calculate -f openfst-tropical \
+    > test.hfst ; then
+    if ! ../../tools/src/hfst-format test.hfst > TMP1 ; then
+	exit 1
+    fi
+    echo "Transducers in test.hfst are of type OpenFST, std arc,"\
+" tropical semiring" > TMP2
+    if ! diff TMP1 TMP2 ; then
+	exit 1
+    fi
 fi
-echo "Transducers in test.hfst are of type SFST (1.4 compatible)" > TMP2
-if ! diff TMP1 TMP2 ; then
-    exit 1
+
+if echo "a:b" | ../../tools/src/hfst-calculate -f foma > test.hfst ; then
+    if ! ../../tools/src/hfst-format test.hfst > TMP1 ; then
+	exit 1
+    fi
+    echo "Transducers in test.hfst are of type foma" > TMP2
+    if ! diff TMP1 TMP2 ; then
+	exit 1
+    fi
 fi
-if ! echo "a:b" | ../../tools/src/hfst-calculate -f openfst-tropical > test.hfst ; then
-    exit 1
-fi
-if ! ../../tools/src/hfst-format test.hfst > TMP1 ; then
-    exit 1
-fi
-echo "Transducers in test.hfst are of type OpenFST, std arc, tropical semiring" > TMP2
-if ! diff TMP1 TMP2 ; then
-    exit 1
-fi
-if ! echo "a:b" | ../../tools/src/hfst-calculate -f foma > test.hfst ; then
-    exit 1
-fi
-if ! ../../tools/src/hfst-format test.hfst > TMP1 ; then
-    exit 1
-fi
-echo "Transducers in test.hfst are of type foma" > TMP2
-if ! diff TMP1 TMP2 ; then
-    exit 1
-fi
+
 rm test.hfst
 rm TMP1
 rm TMP2
--- test/tools/lexc-compiler-functionality.sh
+++ test/tools/lexc-compiler-functionality.sh
@@ -13,21 +13,31 @@ LEXCTESTS="basic.cat-dog-bird.lexc basic.colons.lexc basic.comments.lexc
           xre.definitions.lexc xre.months.lexc xre.nested-definitions.lexc 
           xre.numeric-star.lexc xre.sharp.lexc xre.star-plus-optional.lexc"
 LEXCXFAIL="xfail.bogus.lexc xfail.ISO-8859-1.lexc xfail.lexicon-semicolon.lexc"
-for i in "" .sfst .ofst .foma ; do
+for i in .sfst .ofst .foma ; do
     FFLAG=
+    FNAME=
     case $i in
         .sfst)
-            FFLAG="-f sfst";;
+	    FNAME="sfst";
+            FFLAG="-f sfst";;	
         .ofst)
+	    FNAME="openfst-tropical";
             FFLAG="-f openfst-tropical";;
         .foma)
+	    FNAME="foma";
             FFLAG="-f foma";;
         *)
+	    FNAME=;
             FFLAG=;;
     esac
+
+    if ! (../../tools/src/hfst-format --test-format $FNAME ) ; then
+	continue;
+    fi
+
     if test -f cat.hfst$i ; then
-        if ! ../../tools/src/hfst-lexc2fst $FFLAG $srcdir/cat.lexc > test.hfst ; then
-            echo lexc2fst $FFLAG $srcdir/cat.lexc failed with $?
+        if ! ../../tools/src/hfst-lexc2fst $FFLAG cat.lexc > test.hfst ; then
+            echo lexc2fst $FFLAG cat.lexc failed with $?
             exit 1
         fi
         if ! ../../tools/src/hfst-compare cat.hfst$i test.hfst ; then
@@ -36,16 +46,16 @@ for i in "" .sfst .ofst .foma ; do
         rm test.hfst
     fi
     for f in $LEXCTESTS ; do
-        if ! ../../tools/src/hfst-lexc2fst $FFLAG $srcdir/$f > test.hfst ; then
-            echo lexc2fst $FFLAG $srcdir/$f failed with $?
+        if ! ../../tools/src/hfst-lexc2fst $FFLAG $f > test.hfst ; then
+            echo lexc2fst $FFLAG $f failed with $?
             exit 1
         fi
         rm test.hfst
     done
-    if ! ../../tools/src/hfst-lexc2fst $FFLAG $srcdir/basic.multi-file-1.lexc \
-        $srcdir/basic.multi-file-2.lexc \
-        $srcdir/basic.multi-file-3.lexc > test.hfst ; then
-        echo lexc2fst $FFLAG $srcdir/basic.multi-file-{1,2,3}.lexc failed with $?
+    if ! ../../tools/src/hfst-lexc2fst $FFLAG basic.multi-file-1.lexc \
+        basic.multi-file-2.lexc \
+        basic.multi-file-3.lexc > test.hfst ; then
+        echo lexc2fst $FFLAG basic.multi-file-{1,2,3}.lexc failed with $?
         exit 1
     fi
     if ! ../../tools/src/hfst-compare walk_or_dog.hfst$i test.hfst ; then
