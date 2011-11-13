65b4322d1187e60d2edf63fe5fa37badf6e84952
eaxelson <eaxelson@941e2c2b-deac-454f-805a-451daa25f33c>
2011-11-13 14:33:58 +0000

    Tentatively added to fsmbook-examples tests where the results from foma and xfst are compared.
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@1872 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/fsmbook-examples/foma-att-to-hfst-att.sh
+++ test/tools/fsmbook-examples/foma-att-to-hfst-att.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Convert the att format from foma into the equivalent format of HFST.
+perl -pe "s/\*\*EPSILON\*\*/\@_EPSILON_SYMBOL_\@/g;"
 perl -pe "s/\\\\_/\@_SPACE_\@/g;"  | # space
 perl -pe "s/ /\@_SPACE_\@/g;"  # space
