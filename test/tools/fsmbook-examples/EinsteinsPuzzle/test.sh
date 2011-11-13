4c6eb681bc2cb15684dbe533d82a3e8c030713dc
eaxelson <eaxelson@941e2c2b-deac-454f-805a-451daa25f33c>
2011-11-13 12:13:09 +0000

    Now all tests in fsmbook-examples use the tools in tools/src/ instead of installed tools.
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@1871 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/fsmbook-examples/EinsteinsPuzzle/test.sh
+++ test/tools/fsmbook-examples/EinsteinsPuzzle/test.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 for i in sfst openfst-tropical foma; do
-  bash $1/EinsteinsPuzzle.hfst.script $i
+
+  if ! ($3/hfst-format --test-format $i); then
+      continue;
+  fi
+
+  bash $1/EinsteinsPuzzle.hfst.script $i $3
   diff -q $2/result $1/expected_result;
 done
--- test/tools/fsmbook-examples/FinnishNumerals/test.sh
+++ test/tools/fsmbook-examples/FinnishNumerals/test.sh
@@ -3,9 +3,15 @@
 # sfst composition filter..
 # foma composition filter and same paths printed many times...
 for i in openfst-tropical; do 
-  sh $1/FinnishNumerals.hfst.script $i
-  hfst-fst2strings $2/EnglishToFinnishNumerals.hfst | uniq | sort > $2/TMP1
-  hfst-fst2strings $2/FinnishToEnglishNumerals.hfst | uniq | sort > $2/TMP2
+
+  if ! ($3/hfst-format --test-format $i); then
+      continue;
+  fi
+
+
+  sh $1/FinnishNumerals.hfst.script $i $3
+  $3/hfst-fst2strings $2/EnglishToFinnishNumerals.hfst | uniq | sort > $2/TMP1
+  $3/hfst-fst2strings $2/FinnishToEnglishNumerals.hfst | uniq | sort > $2/TMP2
   diff -q $1/english_to_finnish_numerals_expected $2/TMP1 &&
   diff -q $1/finnish_to_english_numerals_expected $2/TMP2;
 done
