debac1ec0ba84d6febd55ad0dd2ce5160eed7727
eaxelson <eaxelson@941e2c2b-deac-454f-805a-451daa25f33c>
2011-11-13 12:13:09 +0000

    Now all tests in fsmbook-examples use the tools in tools/src/ instead of installed tools.
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@1871 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/fsmbook-examples/Esperanto/test.sh
+++ test/tools/fsmbook-examples/Esperanto/test.sh
@@ -10,15 +10,20 @@ do
 #  echo 'read lexc < "'$i'.lexc"; write att "'$i'" FOO BAR;' | xfst #\
 #      2>1 > /dev/null;
 #  cat $i | ../xfst-att-to-hfst-att.sh > $i.xfst.att;
-  cat $1/$i.xfst.att | hfst-txt2fst -f openfst-tropical > $2/$i.xfst.hfst;
+  cat $1/$i.xfst.att | $3/hfst-txt2fst -f openfst-tropical > $2/$i.xfst.hfst;
 
   # compile with different HFST backends
   for j in sfst openfst-tropical foma;
   do
-    bash $1/$i.hfst.script $j;
+
+    if ! ($3/hfst-format --test-format $i); then
+        continue;
+    fi
+
+    bash $1/$i.hfst.script $j $3;
     # test for equivalence
-    hfst-fst2fst -f openfst-tropical $2/$i.hfst.hfst > $2/TMP;
-    if ! hfst-compare -q $2/TMP $2/$i.xfst.hfst; then
+    $3/hfst-fst2fst -f openfst-tropical $2/$i.hfst.hfst > $2/TMP;
+    if ! $3/hfst-compare -q $2/TMP $2/$i.xfst.hfst; then
       echo "FAIL:" $i "with" $j "failed!";
       exit 1;
     fi;
--- test/tools/fsmbook-examples/NumbersToNumerals/test.sh
+++ test/tools/fsmbook-examples/NumbersToNumerals/test.sh
@@ -5,17 +5,22 @@
 # cat NumbersToNumerals | ../xfst-att-to-hfst-att.sh \
 # > NumbersToNumerals.xfst.att
 
-hfst-txt2fst -f openfst-tropical $1/NumbersToNumerals.xfst.att > \
+$3/hfst-txt2fst -f openfst-tropical $1/NumbersToNumerals.xfst.att > \
   $2/NumbersToNumerals.xfst.hfst
-hfst-project -p input $2/NumbersToNumerals.xfst.hfst > $2/expected_input
-hfst-project -p output $2/NumbersToNumerals.xfst.hfst > $2/expected_output
+$3/hfst-project -p input $2/NumbersToNumerals.xfst.hfst > $2/expected_input
+$3/hfst-project -p output $2/NumbersToNumerals.xfst.hfst > $2/expected_output
 
 for i in sfst openfst-tropical foma; do
-  sh $1/NumbersToNumerals.hfst.script $i
-  hfst-project -p input $2/NumbersToNumerals.hfst.hfst \
-  | hfst-fst2fst -f openfst-tropical > $2/result_input
-  hfst-project -p output $2/NumbersToNumerals.hfst.hfst \
-  | hfst-fst2fst -f openfst-tropical > $2/result_output
-  hfst-compare -q $2/expected_input $2/result_input && \
-  hfst-compare -q $2/expected_input $2/result_input;
+
+  if ! ($3/hfst-format --test-format $i); then
+      continue;
+  fi
+
+  sh $1/NumbersToNumerals.hfst.script $i $3
+  $3/hfst-project -p input $2/NumbersToNumerals.hfst.hfst \
+  | $3/hfst-fst2fst -f openfst-tropical > $2/result_input
+  $3/hfst-project -p output $2/NumbersToNumerals.hfst.hfst \
+  | $3/hfst-fst2fst -f openfst-tropical > $2/result_output
+  $3/hfst-compare -q $2/expected_input $2/result_input && \
+  $3/hfst-compare -q $2/expected_input $2/result_input;
 done
