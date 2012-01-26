b98eae4c3f4196e4527db035c9abdab6692736d2
eaxelson <eaxelson@941e2c2b-deac-454f-805a-451daa25f33c>
2012-01-26 16:39:13 +0000

    Removed spurious consts from optimized lookup conversion functions. Fixes bug ID 3469227.
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@2012 941e2c2b-deac-454f-805a-451daa25f33c

--- libhfst/src/implementations/optimized-lookup/convert.cc
+++ libhfst/src/implementations/optimized-lookup/convert.cc
@@ -102,14 +102,14 @@ void add_transitions_with(SymbolNumber symbol,
     }
 }
 
-const bool compare_states_by_input_size(
+bool compare_states_by_input_size(
     const StatePlaceholder & lhs, const StatePlaceholder & rhs)
 {
     // descending by input size
     return lhs.inputs.size() > rhs.inputs.size();
 }
 
-const bool compare_states_by_state_number(
+bool compare_states_by_state_number(
     const StatePlaceholder & lhs, const StatePlaceholder & rhs)
 {
     // ascending by number
