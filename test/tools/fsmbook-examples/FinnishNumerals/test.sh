a039df96841544159473cf11f8a37b9a5d19381d
eaxelson <eaxelson@941e2c2b-deac-454f-805a-451daa25f33c>
2012-01-26 16:27:20 +0000

    Bug on mac os caused by its sort order (bug ID: 3462485) fixed.
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@2011 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/fsmbook-examples/FinnishNumerals/test.sh
+++ test/tools/fsmbook-examples/FinnishNumerals/test.sh
@@ -12,6 +12,8 @@ for i in openfst-tropical; do
   sh $1/FinnishNumerals.hfst.script $i $3
   $3/hfst-fst2strings $2/EnglishToFinnishNumerals.hfst | uniq | sort > $2/TMP1
   $3/hfst-fst2strings $2/FinnishToEnglishNumerals.hfst | uniq | sort > $2/TMP2
-  diff -q $1/english_to_finnish_numerals_expected $2/TMP1 &&
-  diff -q $1/finnish_to_english_numerals_expected $2/TMP2;
+  cat $1/english_to_finnish_numerals_expected | sort > $2/TMP3
+  cat $1/finnish_to_english_numerals_expected | sort > $2/TMP4
+  diff -q $2/TMP3 $2/TMP1 &&
+  diff -q $2/TMP4 $2/TMP2;
 done
