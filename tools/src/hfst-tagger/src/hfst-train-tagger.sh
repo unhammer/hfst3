11f6ca89098f09d067603a2ddc204c69928b1b9c
mpsilfve <mpsilfve@941e2c2b-deac-454f-805a-451daa25f33c>
2012-06-07 08:39:39 +0000

    Changed comput_data_statistics.py to hfst_tagger_data_statistics.py
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@2436 941e2c2b-deac-454f-805a-451daa25f33c

--- tools/src/hfst-tagger/src/hfst-train-tagger.sh
+++ tools/src/hfst-tagger/src/hfst-train-tagger.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 BINDIR/hfst-open-input-file-for-tagger $@ |
-BINDIR/hfst-tagger-compute_data_statistics.py |
+BINDIR/hfst-tagger-hfst_tagger_compute_data_statistics.py |
 BINDIR/hfst-build-tagger $@
