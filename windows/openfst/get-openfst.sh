2b2b30802c17e4d0d8fc4198e422cd885e69b8fb
eaxelson <eaxelson@941e2c2b-deac-454f-805a-451daa25f33c>
2012-06-27 13:39:08 +0000

    Now all Makefiles should be aware of windows compilation.
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@2521 941e2c2b-deac-454f-805a-451daa25f33c

--- windows/openfst/get-openfst.sh
+++ windows/openfst/get-openfst.sh
@@ -2,28 +2,28 @@
 
 # A script to download openfst
 
-#if ! (mkdir tmp); then
-#    echo "Error when making temporary directory tmp!";
-#    exit 1;
-#fi
+if ! (mkdir tmp); then
+    echo "Error when making temporary directory tmp!";
+    exit 1;
+fi
 
 cd tmp;
 
-#if ! (wget http://openfst.cs.nyu.edu/twiki/pub/Contrib/OpenFstWin/openfstwin-1.3.1.zip); then
-#    echo "Error when retrieving openfst!";
-#    exit 1;
-#fi
+if ! (wget http://openfst.cs.nyu.edu/twiki/pub/Contrib/OpenFstWin/openfstwin-1.3.1.zip); then
+    echo "Error when retrieving openfst!";
+    exit 1;
+fi
 
-#if ! (unzip openfstwin-1.3.1.zip); then
-#    echo "Error when extracting files!";
-#    exit 1;
-#fi
+if ! (unzip openfstwin-1.3.1.zip); then
+    echo "Error when extracting files!";
+    exit 1;
+fi
 
 # test that src/lib exists
 
 cd src/lib;
 
-#cp *.cc ../../../src/lib/
+cp *.cc ../../../src/lib/
 
 cd ../..
 
@@ -31,7 +31,7 @@ cd ../..
 
 cd src/include/fst;
 
-#cp *.h ../../../../src/include/fst
+cp *.h ../../../../src/include/fst
 
 cd ../../..
 
