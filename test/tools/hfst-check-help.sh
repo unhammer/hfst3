d6e7600fbe881f5b6595f323fe71313e1921058e
eaxelson <eaxelson@941e2c2b-deac-454f-805a-451daa25f33c>
2012-09-07 07:40:29 +0000

    Added excutable file extension '.exe' to tests when running them on Windows.
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@2701 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/hfst-check-help.sh
+++ test/tools/hfst-check-help.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
+
+EXT=
+if ! (uname | egrep "Linux|linux" 2>1 > /dev/null); then
+    EXT=".exe";
+fi
+
 for f in ../../tools/src/hfst-* ; do
-    if [ "$f" != "../../tools/src/hfst-lexc2fst" -a \
-        "$f" != "../../tools/src/hfst-xfst2fst" -a \
-	"$f" != "../../tools/src/hfst-lexc-compiler" -a \
-	"$f" != "../../tools/src/hfst-info" ]; then
+    if [ "$f" != "../../tools/src/hfst-lexc2fst""$EXT" -a \
+        "$f" != "../../tools/src/hfst-xfst2fst""$EXT" -a \
+	"$f" != "../../tools/src/hfst-lexc-compiler""$EXT" -a \
+	"$f" != "../../tools/src/hfst-info""$EXT" ]; then
         if [ -x "$f" -a ! -d "$f" ] ; then
             if ! "$f" --help > help.out ; then
                 rm help.out
