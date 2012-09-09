96bcaf14ec2d7b45459d113042d09eaf36adbd71
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2012-09-09 00:30:12 +0000

    Test not contains and multiline expressions with comments and emptylines, Bug [#132]
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@2715 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/regexp2fst-functionality.sh
+++ test/tools/regexp2fst-functionality.sh
@@ -18,6 +18,12 @@ for i in sfst openfst-tropical foma; do
     if ! ../../tools/src/hfst-regexp2fst -f $i $srcdir/at_file_quote.$i.xre > test.fst ; then
         exit 1
     fi
+    if ! ../../tools/src/hfst-regexp2fst -f $i $srcdir/not-contains-a.xre > test.fst ; then
+        exit 1
+    fi
+    if ! ../../tools/src/hfst-regexp2fst -S -f $i $srcdir/not-contains-a-comment-emptyline.xre > test.fst ; then
+        exit 1
+    fi
 done
 
 rm -f test.fst
