e10aef752b5501264a5db50b969662dd6652f0e1
hardwick <hardwick@941e2c2b-deac-454f-805a-451daa25f33c>
2012-09-11 15:18:32 +0000

    Add charset-defining hardcoded transducers to pmatch utilities for now
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@2733 941e2c2b-deac-454f-805a-451daa25f33c

--- libhfst/src/parsers/pmatch_utils.h
+++ libhfst/src/parsers/pmatch_utils.h
@@ -71,6 +71,29 @@ HfstTransducer* compile(const std::string& pmatch,
                         std::map<std::string,hfst::HfstTransducer*>& defs,
                         hfst::ImplementationType type);
 
+/** @brief Return a transducer that accepts one utf-8 symbol that is also a
+    latin-1 alphabetical character. 
+*/
+HfstTransducer latin1_alpha_acceptor(
+    ImplementationType type = TROPICAL_OPENFST_TYPE);
+
+/** @brief Return a transducer that accepts one arabic numeral character. 
+ */
+HfstTransducer latin1_numeral_acceptor(
+    ImplementationType type = TROPICAL_OPENFST_TYPE);
+
+/** @brief Return a transducer that accepts one utf-8 symbol that is also a
+    latin-1 punctuation character.
+*/
+HfstTransducer latin1_punct_acceptor(
+    ImplementationType type = TROPICAL_OPENFST_TYPE);
+
+/** @brief Return a transducer that accepts one utf-8 symbol that is also a
+    latin-1 whitespace character.
+*/
+HfstTransducer latin1_whitespace_acceptor(
+    ImplementationType type = TROPICAL_OPENFST_TYPE);
+
 } } // namespaces
 #endif
 // vim: set ft=cpp.doxygen:
