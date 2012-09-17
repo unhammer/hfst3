359167b455176c8533c81e5a481d7bb4db942110
eaxelson <eaxelson@941e2c2b-deac-454f-805a-451daa25f33c>
2012-09-17 11:15:40 +0000

    Changed ../../tools/src to  in fsmbook-examples to make tests more flexible.
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@2745 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/fsmbook-examples/Esperanto/test.sh
+++ test/tools/fsmbook-examples/Esperanto/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+TOOLDIR=../../tools/src
 
 for i in EsperantoAdjectives \
       	 EsperantoNounsAndAdjectives \
@@ -11,20 +12,20 @@ if [ "$4" = "--test-full" ]; then
 # compile with xfst
     echo 'read lexc < "'$i'.lexc"; write att "'$i'" FOO BAR;' | xfst \
       2>1 > /dev/null;
-    cat $i | ../xfst-att-to-hfst-att.sh > $i.xfst.att;
+    cat $i | ../xfst-att-to-$TOOLDIR/hfst-att.sh > $i.xfst.att;
 fi
 
-  cat $1/$i.xfst.att | $3/hfst-txt2fst -f openfst-tropical > $2/$i.xfst.hfst;
+  cat $1/$i.xfst.att | $3/$TOOLDIR/hfst-txt2fst -f openfst-tropical > $2/$i.xfst.hfst;
 
 if [ "$4" = "--test-full" ]; then 
 # compile with foma
     echo 'read lexc < "'$i'.lexc"; write att "'$i'";' | foma \
       2>1 > /dev/null;
-    cat $i | ../foma-att-to-hfst-att.sh > $i.foma.att;
+    cat $i | ../foma-att-to-$TOOLDIR/hfst-att.sh > $i.foma.att;
 
-    cat $1/$i.foma.att | $3/hfst-txt2fst -f openfst-tropical > $2/$i.foma.hfst;
+    cat $1/$i.foma.att | $3/$TOOLDIR/hfst-txt2fst -f openfst-tropical > $2/$i.foma.hfst;
 
-    if ! $3/hfst-compare -q $2/$i.xfst.hfst $2/$i.foma.hfst; then
+    if ! $3/$TOOLDIR/hfst-compare -q $2/$i.xfst.hfst $2/$i.foma.hfst; then
 	exit 1;
     fi
 fi
@@ -33,14 +34,14 @@ fi
   for j in sfst openfst-tropical foma;
   do
 
-    if ! ($3/hfst-format --test-format $i); then
+    if ! ($3/$TOOLDIR/hfst-format --test-format $i); then
         continue;
     fi
 
     bash $1/$i.hfst.script $j $3;
     # test for equivalence
-    $3/hfst-fst2fst -f openfst-tropical $2/$i.hfst.hfst > $2/TMP;
-    if ! $3/hfst-compare -q $2/TMP $2/$i.xfst.hfst; then
+    $3/$TOOLDIR/hfst-fst2fst -f openfst-tropical $2/$i.hfst.hfst > $2/TMP;
+    if ! $3/$TOOLDIR/hfst-compare -q $2/TMP $2/$i.xfst.hfst; then
       echo "FAIL:" $i "with" $j "failed!";
       exit 1;
     fi;
