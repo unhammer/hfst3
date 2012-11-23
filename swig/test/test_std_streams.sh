4c06d451967ee15c1d84962d8fd15cfa04d25cd5
eaxelson <eaxelson@941e2c2b-deac-454f-805a-451daa25f33c>
2012-11-23 15:23:58 +0000

    Now tests remove extra files that they generate.
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@2867 941e2c2b-deac-454f-805a-451daa25f33c

--- swig/test/test_std_streams.sh
+++ swig/test/test_std_streams.sh
@@ -11,34 +11,53 @@ STRING2="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 echo $STRING1 | $STRINGS2FST -f sfst > tr1.sfst
 echo $STR1NG2 | $STRINGS2FST -f sfst > tr2.sfst
 cat tr1.sfst tr2.sfst > tr.sfst
+rm tr1.sfst tr2.sfst
+
 echo $STRING1 | $STRINGS2FST -f openfst-tropical > tr1.ofst
 echo $STRING2 | $STRINGS2FST -f openfst-tropical > tr2.ofst
 cat tr1.ofst tr2.ofst > tr.ofst
+rm tr1.ofst tr2.ofst
+
 echo $STRING1 | $STRINGS2FST -f foma > tr1.foma
 echo $STRING2 | $STRINGS2FST -f foma > tr2.foma
 cat tr1.foma tr2.foma > tr.foma
+rm tr1.foma tr2.foma
 
 if ! (cat tr.sfst | PYTHONPATH=$PWD/.. $PYTHON ./test_std_streams.py sfst 2 > tmp.sfst); then
     echo "ERROR: in sfst"
+    rm tr.sfst
     exit 1
 fi
+
 if ! ($COMPARE tr.sfst tmp.sfst); then
     echo "ERROR in sfst"
+    rm tr.sfst tmp.sfst
     exit 1
 fi
+rm tr.sfst tmp.sfst
+
 if ! (cat tr.ofst | PYTHONPATH=$PWD/.. $PYTHON ./test_std_streams.py openfst 2 > tmp.ofst); then
     echo "ERROR: in openfst"
+    rm tr.ofst
     exit 1
 fi
+
 if ! ($COMPARE tr.ofst tmp.ofst); then
     echo "ERROR: in openfst"
+    rm tr.ofst tmp.ofst
     exit 1
 fi
+rm tr.ofst tmp.ofst
+
 if ! (cat tr.foma | PYTHONPATH=$PWD/.. $PYTHON ./test_std_streams.py foma 2 > tmp.foma); then
+    rm tr.foma
     echo "ERROR: in foma"
     exit 1
 fi  
+
 if ! ($COMPARE tr.foma tmp.foma); then
     echo "ERROR: in foma"
+    rm tr.foma tmp.foma
     exit 1
 fi  
+rm tr.foma tmp.foma
--- swig/test/test_streams.py
+++ swig/test/test_streams.py
@@ -1,5 +1,10 @@
 import libhfst
 import sys
+import os
+
+def remove_generated_files():
+    os.remove('foo.att')
+    os.remove('foo.hfst')
 
 for ttype in (libhfst.SFST_TYPE, libhfst.TROPICAL_OPENFST_TYPE, libhfst.FOMA_TYPE):
 
@@ -20,10 +25,12 @@ for ttype in (libhfst.SFST_TYPE, libhfst.TROPICAL_OPENFST_TYPE, libhfst.FOMA_TYP
             if transducers_read == 1:
                 if not tr.compare(tr1):
                     print "ERROR: transducer 1 changed."
+                    remove_generated_files()
                     sys.exit(1)
             if transducers_read == 2:
                 if not tr.compare(tr2):
                     print "ERROR: transducer 2 changed."
+                    remove_generated_files()
                     sys.exit(1)
             tr.write_in_att_format(att_file)
             if transducers_read < 2:
@@ -32,6 +39,7 @@ for ttype in (libhfst.SFST_TYPE, libhfst.TROPICAL_OPENFST_TYPE, libhfst.FOMA_TYP
             break
     if transducers_read != 2:
         print "ERROR: wrong number of transducers read"
+        remove_generated_files()
         sys.exit(1)
     att_file.close()
 
@@ -44,14 +52,19 @@ for ttype in (libhfst.SFST_TYPE, libhfst.TROPICAL_OPENFST_TYPE, libhfst.FOMA_TYP
             if transducers_read == 1:
                 if not tr.compare(tr1):
                     print "ERROR: transducer 1 changed in ATT format."
+                    remove_generated_files()
                     sys.exit(1)
             if transducers_read == 2:
                 if not tr.compare(tr2):
                     print "ERROR: transducer 2 changed in ATT format."
+                    remove_generated_files()
                     sys.exit(1)
         except libhfst.EndOfStreamException:
             break
     if transducers_read != 2:
         print "ERROR: wrong number of ATT transducers read"
+        remove_generated_files()
         sys.exit(1)
     att_file.close()
+
+remove_generated_files()
