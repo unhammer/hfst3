20965ab6e1888fe9f0b349141b4bf1b8014fbc4c
eaxelson <eaxelson@941e2c2b-deac-454f-805a-451daa25f33c>
2013-01-09 16:01:06 +0000

    Updated README. Fixed hfst-info so that it passes version and help checks.
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@2951 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/hfst-check-help.sh
+++ test/tools/hfst-check-help.sh
@@ -7,10 +7,9 @@ if (uname | egrep "MINGW|mingw" 2>1 > /dev/null); then
 fi
 
 for f in $TOOLDIR/hfst-* ; do
-    if [ "$f" != "$TOOLDIR/hfst-lexc2fst""$EXT" -a \
-        "$f" != "$TOOLDIR/hfst-xfst2fst""$EXT" -a \
+    # Skip deprecated tools that can still be installed somewhere
+    if [ "$f" != "$TOOLDIR/hfst-xfst2fst""$EXT" -a \
 	"$f" != "$TOOLDIR/hfst-lexc-compiler""$EXT" -a \
-	"$f" != "$TOOLDIR/hfst-info""$EXT" -a \
 	"$f" != "$TOOLDIR/hfst-twolc-system""$EXT" -a \
 	"$f" != "$TOOLDIR/hfst-duplicate""$EXT" -a \
 	"$f" != "$TOOLDIR/hfst-preprocess-for-optimized-lookup-format""$EXT" -a \
