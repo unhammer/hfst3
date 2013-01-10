d52ac7784d97d7af9fe0d65226203e50df9d247c
sdrobac <sdrobac@941e2c2b-deac-454f-805a-451daa25f33c>
2013-01-10 14:23:26 +0000

    Fsm-book test Palindromes updated
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@2954 941e2c2b-deac-454f-805a-451daa25f33c

---
+++ test/tools/fsmbook-examples/palindromes.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Perform check in each directory.
+# Rules must be implemented before all directories can be tested. 
+# Directory Lingala requires a professional licence of xfst so that
+# the resulting transducer can be written in att format. 
+
+
+cd Palindromes;
+if make check; then
+    cd ..;
+    exit 0;
+fi;
+exit 1;
+
+
+# We usually have in each file "Foo"
+# - an xfst script "Foo.xfst.script" that is basically the xfst file found
+#   in the fsmbook web page
+# - either (1) a ready-compiled AT&T format representation "Foo.xfst.att" or 
+#   (2) a string (pair) representation "expected_strings" of the transducer 
+#   that is obtained when executing the script "Foo.xfst.script" 
+# - an HFST script "Foo.hfst.script" that is compiled into a transducer
+#   using all HFST implementation types.
+# - a Makefile "Makefile.am"
+# - a test file test.script that is executed by "Makefile.am" when make check
+#   is done
+# 
+# For each HFST implementation type, we test if the result from 
+# "Foo.hfst.script" is equivalent to "Foo.xfst.att" or "expected_strings".
