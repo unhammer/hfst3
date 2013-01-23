b906045a7c30853d86a96cb3acf76c6acba49d29
eaxelson <eaxelson@941e2c2b-deac-454f-805a-451daa25f33c>
2013-01-23 13:35:06 +0000

    Made a local version of hfst-xfst that is used in tests.
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@2986 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/xfst-functionality.sh
+++ test/tools/xfst-functionality.sh
@@ -2,7 +2,7 @@
 SCRIPTDIR=../../scripts/
 TOOLDIR=../../tools/src/
 
-if ! test -x $SCRIPTDIR/hfst-xfst; then
+if ! test -x $SCRIPTDIR/hfst-xfst-loc; then
     exit 0;
 fi
 
@@ -10,12 +10,12 @@ for format in sfst openfst-tropical foma; do
     
     if ($TOOLDIR/hfst-format --list-formats | grep $format > /dev/null) ; then
 
-	if ! ($SCRIPTDIR/hfst-xfst -F $format -f script.xfst); then
+	if ! ($SCRIPTDIR/hfst-xfst-loc -F $format -f script.xfst) 2>1 > /dev/null; then
 	    rm -f script.hfst
 	    exit 1;
 	fi
 
-	if ! echo "a:b" | $TOOLDIR/hfst-strings2fst -f $format | $TOOLDIR/hfst-compare script.hfst; then
+	if ! echo "a:b" | $TOOLDIR/hfst-strings2fst -f $format | $TOOLDIR/hfst-compare -s script.hfst; then
 	    rm -f script.hfst
 	    exit 1;
 	fi
