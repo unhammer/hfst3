d658d2efecff04805e71516e7943c1f3dc3ae5d1
eaxelson <eaxelson@941e2c2b-deac-454f-805a-451daa25f33c>
2013-02-22 10:48:10 +0000

    Added tests for hfst-xfst2fst.
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@3041 941e2c2b-deac-454f-805a-451daa25f33c

---
+++ tools/src/parsers/test/test.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+XFST_TOOL="../hfst-xfst2fst -s"
+STRINGS2FST="../../hfst-strings2fst -S"
+COMPARE="../../hfst-compare --quiet"
+FORMATS="../../hfst-format --list-formats"
+EXTRA_FILES="tmp startup"
+REMOVE="rm -f"
+
+for format in sfst openfst-tropical foma;
+do
+
+    if ! (${FORMATS} --list-formats | grep $format 2>1 > /dev/null); then
+	continue;
+    fi
+    
+    echo "define Foo foo;" > startup
+    if ! (echo "regex Foo Bar; save stack tmp;" | ${XFST_TOOL} -l startup -e "define Bar bar;"); then
+	${REMOVE} ${EXTRA_FILES}
+	exit 1
+    fi
+    if ! (echo "foo bar" | ${STRINGS2FST} -S | ${COMPARE} tmp); then
+	${REMOVE} ${EXTRA_FILES}
+	exit 1
+    fi
+    
+done
+
+${REMOVE} ${EXTRA_FILES}
+exit 0;
