a2146ab70538304cad996e0b2f042e4020a06049
eaxelson <eaxelson@941e2c2b-deac-454f-805a-451daa25f33c>
2013-02-25 13:24:52 +0000

    Now all xfst files are read in a buffer and parsed with XfstCompiler.parse_line.
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@3044 941e2c2b-deac-454f-805a-451daa25f33c

--- tools/src/parsers/test/test.sh
+++ tools/src/parsers/test/test.sh
@@ -3,25 +3,49 @@
 XFST_TOOL="../hfst-xfst2fst -s"
 STRINGS2FST="../../hfst-strings2fst -S"
 COMPARE="../../hfst-compare --quiet"
-FORMATS="../../hfst-format --list-formats"
+LIST_FORMATS="../../hfst-format --list-formats"
 EXTRA_FILES="tmp startup"
 REMOVE="rm -f"
 
 for format in sfst openfst-tropical foma;
 do
-    if ! (${FORMATS} --list-formats | grep $format > /dev/null); then
+    # Test if implementation type is available.
+    if ! (${LIST_FORMATS} | grep $format > /dev/null); then
 	continue;
     fi
-    echo "define Foo foo" > startup
-    echo ";" >> startup
-    if ! ((echo "regex Foo Bar;" && echo "save stack tmp") | ${XFST_TOOL} -f $format -l startup -e "define Bar bar;"); then
+
+    # Create a transducer [Foo Bar Baz] where Foo is [foo], Bar [bar] and Baz [baz].
+    # Definition of Foo is given in startup file, and definitions of Bar and Baz
+    # on command line.
+    echo "define Foo" > startup # continue regex on
+    echo "foo;" >> startup      # another line
+    if ! ((echo "regex Foo Bar Baz;" && echo "save stack tmp") | \
+	${XFST_TOOL} -f $format -l startup \
+	-e "define Bar bar;" -e "define Baz baz;");
+    then
 	${REMOVE} ${EXTRA_FILES}
 	exit 1
     fi
-    if ! (echo "foo bar" | ${STRINGS2FST} -f $format -S | ${COMPARE} tmp); then
+    # Test that the result is as intended.
+    if ! (echo "foo bar baz" | ${STRINGS2FST} -f $format | ${COMPARE} tmp);
+    then
 	${REMOVE} ${EXTRA_FILES}
 	exit 1
     fi
+
+    # Create a transducer with literal words "define".
+    if ! ((echo "regex define;" && echo "save stack tmp") | ${XFST_TOOL} -f $format;);
+    then
+	${REMOVE} ${EXTRA_FILES}
+	exit 1
+    fi
+    # Test that the result is as intended.
+    if ! (echo "define" | ${STRINGS2FST} -f $format | ${COMPARE} tmp);
+    then
+	${REMOVE} ${EXTRA_FILES}
+	exit 1
+    fi
+    
 done
 
 ${REMOVE} ${EXTRA_FILES}
