83080a3ff66072cf88956237b64d5c74e2a24519
eaxelson <eaxelson@941e2c2b-deac-454f-805a-451daa25f33c>
2013-03-22 15:22:44 +0000

    Documented flag elimination function. Fixed some bugs in using transducer definitions.
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@3162 941e2c2b-deac-454f-805a-451daa25f33c

--- libhfst/src/parsers/xre_utils.h
+++ libhfst/src/parsers/xre_utils.h
@@ -68,6 +68,8 @@ HfstTransducer* compile(const std::string& xre,
  */
 HfstTransducer* expand_definition(HfstTransducer* tr, const char* symbol);
 
+bool is_definition(const char* symbol);
+
 /** @brief Parse "input:output", ":output", "input:" or ":". */
  HfstTransducer* xfst_label_to_transducer(const char* input, const char* output);
 
