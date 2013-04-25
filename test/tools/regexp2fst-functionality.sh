d625c3e12a43be9117c657a6877fe0ad68f3827b
moshagen <moshagen@941e2c2b-deac-454f-805a-451daa25f33c>
2013-04-25 08:17:40 +0000

    Added test case for bug https://sourceforge.net/p/hfst/bugs/177/ .
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@3246 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/regexp2fst-functionality.sh
+++ test/tools/regexp2fst-functionality.sh
@@ -31,6 +31,9 @@ for i in sfst openfst-tropical foma; do
     if ! $TOOLDIR/hfst-regexp2fst -S -f $i $srcdir/parallel-left-arrow-multicom-emptyline.xre > test.fst ; then
         exit 1
     fi
+    if ! $TOOLDIR/hfst-regexp2fst -S -f $i $srcdir/left-arrow-with-semicolon-comment.xre > test.fst ; then
+        exit 1
+    fi
 done
 
 rm -f test.fst
