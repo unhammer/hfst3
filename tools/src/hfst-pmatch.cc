6ad6ab27391e0df8536ffee65e3cb9702cd5fc86
hardwick <hardwick@941e2c2b-deac-454f-805a-451daa25f33c>
2013-05-15 11:48:18 +0000

    Reworking of pmatch tool, added default of blank line as separator
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@3300 941e2c2b-deac-454f-805a-451daa25f33c

--- tools/src/hfst-pmatch.cc
+++ tools/src/hfst-pmatch.cc
@@ -1,6 +1,55 @@
-#include "hfst-pmatch.h"
+//! @file hfst-pmatch.cc
+//!
+//! @brief Pmatch utility for continuous matching on input streams
+//!
+//! @author HFST Team
 
-std::string input_file_name;
+
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, version 3 of the License.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+
+#include <iostream>
+#include <fstream>
+
+#include <vector>
+#include <map>
+#include <string>
+
+using std::string;
+using std::vector;
+using std::pair;
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <getopt.h>
+#include <math.h>
+#include <errno.h>
+
+#include "hfst-commandline.h"
+#include "hfst-program-options.h"
+#include "hfst-tool-metadata.h"
+#include "implementations/optimized-lookup/pmatch.h"
+
+#include "inc/globals-common.h"
+#include "inc/globals-unary.h"
+
+bool blankline_separated = true;
+std::string pmatch_filename;
 
 void
 print_usage()
@@ -9,169 +58,157 @@ print_usage()
     fprintf(message_out, "Usage: %s [OPTIONS...] TRANSDUCER\n"
             "perform matching/lookup on text streams\n"
             "\n", program_name);
-
     print_common_program_options(message_out);
+    fprintf(message_out,
+            "  -n  --newline          Newline as input separator (default is blank line)\n");
     fprintf(message_out, 
-            "Input/Output options:\n"
-            "  -i, --input=INFILE     Read input transducer from INFILE\n"
-            "  -o, --output=OUTFILE   Write output to OUTFILE\n");
-
-    fprintf(message_out, "Pmatch options:\n");
-//            "  -e, --epsilon-format=EPS         Print epsilon as EPS\n"
-    fprintf(message_out, "\n");
-    print_common_unary_program_parameter_instructions(message_out);
-    fprintf(message_out, "\n");
-    fprintf(message_out, "\n");
+            "Use standard streams for input and output.\n"
+            "\n"
+        );
+    
     print_report_bugs();
     fprintf(message_out, "\n");
     print_more_info();
+    fprintf(message_out, "\n");
 }
 
-// hfst_ol::PmatchContainer process_transducers(std::ifstream & inputstream)
-// {
-//     hfst_ol::PmatchContainer container(inputstream);
-//     while (inputstream.good()) {
-// try {
-//     transducer_name = parse_hfst3_header(inputstream);
-// } catch (TransducerHeaderException & e) {
-//     break;
-// }
-//         container.add_rtn(
-//     new hfst_ol::PmatchTransducer(inputstream, false), transducer_name);
+int process_input(hfst_ol::PmatchContainer & container,
+                  std::ostream & outstream)
+{
+    std::string input_text;
+    char * line = NULL;
+    size_t len = 0;
+    while (hfst_getline(&line, &len, inputfile) > 0) {
+        if (!blankline_separated) {
+            // newline separated
+            input_text = line;
+            outstream << container.match(input_text);
+        } else if (line[0] == '\n') {
+            outstream << container.match(input_text);
+            input_text.clear();
+        } else {
+            input_text.append(line);
+        }
+        free(line);
+        line = NULL;
+    }
+    if (!input_text.empty()) {
+        outstream << container.match(input_text);
+    }
+//         if (c == '\n') {
+//             if (blankline_separated) {
+//                 if (input_text[input_text.size() - 1] == '\n') {
+//                     std::cerr << "Sending " << input_text << std::endl;
+//                     outstream << container.match(input_text);
+//                     input_text.clear();
+//                     input_line.clear();
+//                 } else {
+//                     // Just a newline, not a blank line
+//                     input_line.push_back(c);
+//                     input_text.append(input_line);
+//                     input_line.clear();
+//                 }
+//             } else {
+//                 // newline separated
+//                 outstream << container.match(input_line);
+//                 input_line.clear();
+//             }
+//         } else {
+//             // a regular char
+//             input_line.push_back(c);
+//         }
 //     }
-//     find_rtns(container);
-//     return container;
-// }
-
-void print_result(std::string res)
-{
-    std::cout << res << std::endl;;
+    return EXIT_SUCCESS;
 }
 
-void process_input(hfst_ol::PmatchContainer & container)
-{
-    std::string input_line;
-    while (std::cin) {
-        getline(std::cin, input_line);
-        print_result(container.match(input_line));
-    }
-
-    // std::istream user_input = std::cin;
-    // while(!getline(user_input, input_line).failbit) {
-    //     print_result(container.match(input_line));
-    // }
-
-}
-
-
-// std::map<std::string, std::string> get_possible_transducers_from_path()
-// {
-//     return std::map<std::string, std::string>();
-// //     char tmp[MAXPATHLEN];
-// //     std::string cwd_name = getcwd(tmp, MAXPATHLEN) ?
-// //         std::string(tmp) : std::string("");
-
-// }
-
-void find_rtns(hfst_ol::PmatchContainer & cont)
-{
-    if (cont.has_unsatisfied_rtns()) {
-        error(EXIT_FAILURE, 0,
-              "Couldn't find referred transducer"); 
-    }
-    // std::map<std::string, std::string> transducer_library =
-    //     get_possible_transducers_from_path();
-    // std::map<std::string, std::string>::iterator it;
-    // while (cont.has_unsatisfied_rtns()) {
-    //     it = transducer_library.find(cont.get_unsatisfied_rtn_name());
-    //     if (it != transducer_library.end()) {
-    //         HfstInputStream in(it->second);
-    //         HfstTransducer rtn(in);
-    //         force_ol(rtn);
-    //         cont.add_rtn(rtn);
-    //     } else {
-    //         error(EXIT_FAILURE, 0,
-    //               "Couldn't find referred transducer");
-    //     }
-    // }
-
-}
 
 int parse_options(int argc, char** argv)
 {
-    int c;
+    extend_options_getenv(&argc, &argv);
+    // use of this function requires options are settable on global scope
     while (true)
     {
         static const struct option long_options[] =
             {
-                // first the hfst-mandated options
-                {"help",         no_argument,       0, 'h'},
-                {"version",      no_argument,       0, 'V'},
-                {"verbose",      no_argument,       0, 'v'},
-                {"quiet",        no_argument,       0, 'q'},
-                {"silent",       no_argument,       0, 's'},
+                HFST_GETOPT_COMMON_LONG,
+                {"newline", no_argument, 0, 'n'},
                 {0,0,0,0}
             };
         int option_index = 0;
-        // add tool-specific options here 
-        c = getopt_long(argc, argv,
-                        "hVvqs",
-                        long_options, &option_index);
+        char c = getopt_long(argc, argv, HFST_GETOPT_COMMON_SHORT "n",
+                             long_options, &option_index);
         if (-1 == c)
         {
             break;
         }
 
+
         switch (c)
         {
-        case 'h':
-            print_usage();
-            return EXIT_SUCCESS;
+#include "inc/getopt-cases-common.h"
+        case 'n':
+            blankline_separated = false;
             break;
-        case 'V':
-          print_version();
-          return EXIT_SUCCESS;
-          break;
-        default:
-            std::cerr << "Invalid option\n\n";
-            return EXIT_FAILURE;
-            break;
+#include "inc/getopt-cases-error.h"
         }
+
+        
+        
     }
 
-    // no more options, we should now be at the input filename
-    if ( (optind + 1) < argc)
-    {
-        std::cerr << "More than one input file given\n";
-        return EXIT_FAILURE;
-    }
-    else if ( (optind + 1) == argc)
-    {
-        input_file_name = argv[(optind)];
-        return EXIT_CONTINUE;
-    }
-    else
-    {
-        std::cerr << "No input file given\n";
-        return EXIT_FAILURE;
-    }
+//            if (!inputNamed)
+//        {
+//            inputfile = stdin;
+//            inputfilename = hfst_strdup("<stdin>");
+//        }
+        
+        // no more options, we should now be at the input filename
+        if ( (optind + 1) < argc)
+        {
+            std::cerr << "More than one input file given\n";
+            return EXIT_FAILURE;
+        }
+        else if ( (optind + 1) == argc)
+        {
+            pmatch_filename = argv[(optind)];
+            return EXIT_CONTINUE;
+        }
+        else
+        {
+            std::cerr << "No input file given\n";
+            return EXIT_FAILURE;
+        }
 
-    return EXIT_CONTINUE;
+
+#include "inc/check-params-common.h"
+
+
+    
+    return EXIT_FAILURE;
 }
 
 
 int main(int argc, char ** argv)
 {
-    hfst_setlocale();
     hfst_set_program_name(argv[0], "0.1", "HfstPmatch");
+    hfst_setlocale();
     int retval = parse_options(argc, argv);
     if (retval != EXIT_CONTINUE) {
         return retval;
     }
-    std::ifstream instream(input_file_name.c_str(),
+    std::ifstream instream(pmatch_filename.c_str(),
                            std::ifstream::binary);
+    if (!instream.good()) {
+        std::cerr << "Could not open file " << pmatch_filename << std::endl;
+        return EXIT_FAILURE;
+    }
     hfst_ol::PmatchContainer container(instream);
-
-    process_input(container);
-    return EXIT_SUCCESS;
+//     if (outfile != stdout) {
+//         std::filebuf fb;
+// fb.open(outfilename, std::ios::out);
+// std::ostream outstream(&fb);
+// return process_input(container, outstream);
+// fb.close();
+//     } else {
+    return process_input(container, std::cout);
 }
