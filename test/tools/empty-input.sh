322027a3603ea3176e511d31a8885c9514b220f2
eaxelson <eaxelson@941e2c2b-deac-454f-805a-451daa25f33c>
2013-05-22 12:40:41 +0000

    Tests for version and help checking as well as empty input now check only a set of listed tools instead of hfst-*.
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@3319 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/empty-input.sh
+++ test/tools/empty-input.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
+
 TOOLDIR=../../tools/src
-for f in $TOOLDIR/hfst-* ; do
+
+HFST_TOOLS="hfst-affix-guessify hfst-calculate hfst-compare hfst-compose \
+hfst-compose-intersect hfst-concatenate hfst-conjunct hfst-determinize \
+hfst-disjunct hfst-edit-metadata hfst-format hfst-fst2fst \
+hfst-fst2strings hfst-fst2txt hfst-grep hfst-guess hfst-guessify \
+hfst-head hfst-info hfst-invert hfst-lexc hfst-lexc2fst \
+hfst-lookup hfst-minimize hfst-multiply hfst-name \
+hfst-optimized-lookup hfst-pair-test hfst-pmatch hfst-pmatch2fst \
+hfst-project hfst-prune-alphabet hfst-push-weights hfst-regexp2fst \
+hfst-remove-epsilons hfst-repeat hfst-reverse hfst-reweight \
+hfst-shuffle hfst-split hfst-strings2fst hfst-substitute \
+hfst-subtract hfst-summarize hfst-tail hfst-traverse \
+hfst-txt2fst"
+
+for prog in $HFST_TOOLS ; do
+    f=$TOOLDIR/$prog
     if [ -x "$f" -a ! -d "$f" ] ; then
         $f < $srcdir/empty-file > /dev/null 2>&1
         rv=$?
