6fa0133871e09f4fc875f083ee93ea369c32b564
eaxelson <eaxelson@941e2c2b-deac-454f-805a-451daa25f33c>
2013-10-31 08:38:54 +0000

    Fixed a typo in test script.
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@3600 941e2c2b-deac-454f-805a-451daa25f33c

--- check_installation/test-hfst-xfst.sh
+++ check_installation/test-hfst-xfst.sh
@@ -5,7 +5,7 @@
 
 if ! [ -x $1/hfst-xfst ]; then
     echo "warning: hfst-xfst not found, assumed switched off and skipping it"
-    exit 0
+    exit 77
 fi
 
 rm -f a2b.script
@@ -15,7 +15,7 @@ echo "save stack a2b.xfst.hfst" >> a2b.script
 for format in sfst openfst-tropical foma;
 do
     # Check that the tool is installed right..
-    if ! ($1/hfst-xfst -f a2b.script -F $format 2>1 > /dev/null); then
+    if ! ($1/hfst-xfst -F a2b.script -f $format 2>1 > /dev/null); then
 	exit 1
     fi
     # Check that the tool produces right result..
