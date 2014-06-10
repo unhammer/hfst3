5fca6d6fdef8660ea57cddffab01a9f7e4aef4d7
mpsilfve <mpsilfve@941e2c2b-deac-454f-805a-451daa25f33c>
2014-06-10 13:45:55 +0000

    Fixed list center rules and conflict resolutions.
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk/hfst3@3921 941e2c2b-deac-454f-805a-451daa25f33c

--- tools/src/hfst-twolc/src/variable_src/RuleSymbolVector.cc
+++ tools/src/hfst-twolc/src/variable_src/RuleSymbolVector.cc
@@ -50,6 +50,39 @@ std::string RuleSymbolVector::replace_variables(void)
   return result;
 }
 
+std::string RuleSymbolVector::replace_variables(const RuleCenter &center)
+{
+  std::string result;
+  for (std::vector<std::string>::const_iterator it = 
+         std::vector<std::string>::begin();
+       it != std::vector<std::string>::end();
+       ++it)
+    {
+      std::string symbol = *it;
+      if (symbol.find("__HFST_TWOLC_RULE_NAME") != std::string::npos)
+    {
+      if (not vvm.empty())
+        {
+          symbol.insert
+        (symbol.size()-1,
+         "__HFST_TWOLC_SPACE" "SUBCASE:");
+        }
+      for (VariableValueMap::const_iterator it = vvm.begin();
+           it != vvm.end();
+           ++it)
+        { symbol.insert
+        (symbol.size()-1,
+         "__HFST_TWOLC_SPACE"+ it->first + "=" + it->second); }
+    }
+      else if (symbol == "__HFST_TWOLC_RULE_CENTER")
+        { symbol = center.first + " __HFST_TWOLC_: " + center.second; }
+
+      result += 
+    (vvm.has_key(symbol) ? vvm.get_value(symbol) : symbol) + " ";
+    }
+  return result;
+}
+
 RuleSymbolVector &RuleSymbolVector::push_back(const std::string &s)
 { 
   std::vector<std::string>::push_back(s); 
