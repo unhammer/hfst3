b27defdb1c832bb0829fbe346e4efb13e63d890e
eaxelson <eaxelson@941e2c2b-deac-454f-805a-451daa25f33c>
2015-12-11 15:25:14 +0000

    Add tests where one or both inputs are archives.
    git-svn-id: svn+ssh://svn.code.sf.net/p/hfst/code/trunk/hfst3@4575 941e2c2b-deac-454f-805a-451daa25f33c

--- test/tools/mismatching-input-streams.sh
+++ test/tools/mismatching-input-streams.sh
@@ -12,11 +12,13 @@ do
     do
         for tool in $TOOLS;
         do
+            # only test inputs that have mismatching types
             if (test "$type1" != "$type2"); then
                 if (ls $TOOLDIR/$tool > /dev/null 2> /dev/null); then
                     # echo "$tool"
                     if ($TOOLDIR/hfst-format --list-formats | grep $type1 > /dev/null); then
                         if ($TOOLDIR/hfst-format --list-formats | grep $type2 > /dev/null); then
+                            # (1) two transducers
                             if test -f cat.$type1 -a -f dog.$type2; then
                                 # echo "  $type1, $type2"
                                 # echo "    normal"
@@ -36,6 +38,34 @@ do
                                 fi
                                 # echo "    strict"
                                 if $TOOLDIR/$tool $STRICTOPT cat.$type2 dog.$type1 > test 2> /dev/null ; then
+                                    exit 1
+                                fi
+                            fi
+                            # (2) archives of transducers with equal length
+                            if test -f cat.$type1 -a -f cat.$type2 -a -f dog.$type1 -a -f dog.$type2; then
+                                cat cat.$type1 dog.$type1 > test1
+                                cat cat.$type2 dog.$type2 > test2
+                                # echo "  archive $type1, archive $type2"
+                                # echo "    normal"
+                                if ! $TOOLDIR/$tool $NORMOPT test1 test2 > test 2> /dev/null ; then
+                                    exit 1
+                                fi
+                                # echo "    strict"
+                                if $TOOLDIR/$tool $STRICTOPT test1 test2 > test 2> /dev/null ; then
+                                    exit 1
+                                fi
+                            fi
+                            # (3) archives of transducers with different lengths
+                            if test -f cat.$type1 -a -f cat.$type2 -a -f dog.$type1 -a -f dog.$type2; then
+                                cat cat.$type1 dog.$type1 cat.$type1 dog.$type1 > test1
+                                cat cat.$type2 > test2
+                                # echo "  archive $type1, $type2"
+                                # echo "    normal"
+                                if ! $TOOLDIR/$tool $NORMOPT test1 test2 > test 2> /dev/null ; then
+                                    exit 1
+                                fi
+                                # echo "    strict"
+                                if $TOOLDIR/$tool $STRICTOPT test1 test2 > test 2> /dev/null ; then
                                     exit 1
                                 fi
                             fi
